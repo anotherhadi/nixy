900eb52b17a7c51b64aef04bb25ce8aa66ae84c7
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-03-18 22:37:03 +0100

    Fix windows name & padding
    Former-commit-id: 1dd92201d04d6e284ba9015fcf0b59aa01dca3ef

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -117,142 +117,142 @@
       };
     };
     style = ''
-            * {
-              border: none;
-              border-radius: 0;
-              min-height: 0;
-              font-family: "${config.theme.font}";
-              color: #${config.theme.colors.fg};
-              font-weight: 700;
-            }
-
-            window#waybar {
-              background-color: ${
-                if config.theme.waybar-transparent then
-                  "rgba(0, 0, 0, 0)"
-                else
-                  "#${config.theme.colors.bg}"
-              };
-              transition-property: background-color;
-              transition-duration: 0.5s;
-              border-radius: ${toString config.theme.rounding}px;
-              font-size: 13px;
-            }
-
-            .modules-left, .modules-center, .modules-right {
-              border-radius: ${toString config.theme.rounding}px;
-              background-color: #${config.theme.colors.bg};
-              padding: 2px 15px;
-            }
-
-            window#waybar.hidden {
-              opacity: 0.5;
-            }
-
-            #workspaces {
-              background-color: transparent;
-            }
-
-            #workspaces button {
-              all: initial; /* Remove GTK theme values (waybar #1351) */
-              min-width: 0; /* Fix weird spacing in materia (waybar #450) */
-              box-shadow: inset 0 -3px transparent; /* Use box-shadow instead of border so the text isn't offset */
-              padding: 6px 18px;
-              margin: 6px 3px;
-              border-radius: 4px;
-              background-color: #${config.theme.colors.alt-bg};
-              color: #${config.theme.colors.alt-fg};
-            }
-
-            #workspaces button.active {
-              color: #${config.theme.colors.primary-fg};
-              background-color: #${config.theme.colors.primary-bg};
-            }
-
-            #workspaces button:hover {
-             box-shadow: inherit;
-             text-shadow: inherit;
-             opacity: 0.8;
-            }
-
-            #workspaces button.urgent {
-              background-color: #${config.theme.colors.color1};
-            }
-
-      #window {
-              font-family: "${config.theme.font-mono}";
+      * {
+        border: none;
+        border-radius: 0;
+        min-height: 0;
+        font-family: "${config.theme.font}";
+        color: #${config.theme.colors.fg};
+        font-weight: 700;
       }
 
-            #memory,
-            #custom-power,
-            #battery,
-            #backlight,
-            #pulseaudio,
-            #network,
-            #clock,
-            #tray,
-            #backlight{
-              border-radius: 9px;
-              margin: 6px 3px;
-              padding: 6px 12px;
-              background-color: #${config.theme.colors.alt-bg};
-              color: #${config.theme.colors.alt-fg};
-            }
+      window#waybar {
+        background-color: ${
+          if config.theme.waybar-transparent then
+            "rgba(0, 0, 0, 0)"
+          else
+            "#${config.theme.colors.bg}"
+        };
+        transition-property: background-color;
+        transition-duration: 0.5s;
+        border-radius: ${toString config.theme.rounding}px;
+        font-size: 13px;
+      }
 
-            #tray menu {
-              background-color: #${config.theme.colors.alt-bg};
-              color: #${config.theme.colors.alt-fg};
-            }
+      .modules-left, .modules-center, .modules-right {
+        border-radius: ${toString config.theme.rounding}px;
+        background-color: #${config.theme.colors.bg};
+        padding: 2px 6px;
+      }
 
-            #custom-logo {
-              padding-right: 7px;
-              font-size: 15px;
-              color: #${config.theme.colors.primary-bg};
-            }
+      window#waybar.hidden {
+        opacity: 0.5;
+      }
 
-            @keyframes blink {
-              to {
-                background-color: #f38ba8;
-                color: #181825;
-              }
-            }
+      #workspaces {
+        background-color: transparent;
+      }
 
-            #battery.warning,
-            #battery.critical,
-            #battery.urgent {
-              background-color: #ff0048;
-              color: #181825;
-              animation-name: blink;
-              animation-duration: 0.5s;
-              animation-timing-function: linear;
-              animation-iteration-count: infinite;
-              animation-direction: alternate;
-            }
+      #workspaces button {
+        all: initial; /* Remove GTK theme values (waybar #1351) */
+        min-width: 0; /* Fix weird spacing in materia (waybar #450) */
+        box-shadow: inset 0 -3px transparent; /* Use box-shadow instead of border so the text isn't offset */
+        padding: 6px 18px;
+        margin: 6px 3px;
+        border-radius: 4px;
+        background-color: #${config.theme.colors.alt-bg};
+        color: #${config.theme.colors.alt-fg};
+      }
 
-            #battery.charging {
-              background-color: #${config.theme.colors.alt-bg};
-              color: #${config.theme.colors.alt-fg};
-              animation: none;
-            }
+      #workspaces button.active {
+        color: #${config.theme.colors.primary-fg};
+        background-color: #${config.theme.colors.primary-bg};
+      }
 
-            #custom-power {
-              background-color: #${config.theme.colors.primary-bg};
-              color: #${config.theme.colors.primary-fg};
-            }
+      #workspaces button:hover {
+       box-shadow: inherit;
+       text-shadow: inherit;
+       opacity: 0.8;
+      }
+
+      #workspaces button.urgent {
+        background-color: #${config.theme.colors.color1};
+      }
+
+      #window > * {
+        font-family: "${config.theme.font-mono}";
+      }
+
+      #memory,
+      #custom-power,
+      #battery,
+      #backlight,
+      #pulseaudio,
+      #network,
+      #clock,
+      #tray,
+      #backlight{
+        border-radius: 9px;
+        margin: 6px 3px;
+        padding: 6px 12px;
+        background-color: #${config.theme.colors.alt-bg};
+        color: #${config.theme.colors.alt-fg};
+      }
+
+      #tray menu {
+        background-color: #${config.theme.colors.alt-bg};
+        color: #${config.theme.colors.alt-fg};
+      }
+
+      #custom-logo {
+        padding-right: 7px;
+        font-size: 15px;
+        color: #${config.theme.colors.primary-bg};
+      }
+
+      @keyframes blink {
+        to {
+          background-color: #f38ba8;
+          color: #181825;
+        }
+      }
+
+      #battery.warning,
+      #battery.critical,
+      #battery.urgent {
+        background-color: #ff0048;
+        color: #181825;
+        animation-name: blink;
+        animation-duration: 0.5s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+        animation-direction: alternate;
+      }
+
+      #battery.charging {
+        background-color: #${config.theme.colors.alt-bg};
+        color: #${config.theme.colors.alt-fg};
+        animation: none;
+      }
+
+      #custom-power {
+        background-color: #${config.theme.colors.primary-bg};
+        color: #${config.theme.colors.primary-fg};
+      }
 
 
-            tooltip {
-              border-radius: 8px;
-              padding: 15px;
-              background-color: #${config.theme.colors.alt-bg};
-              color: #${config.theme.colors.alt-fg};
-            }
+      tooltip {
+        border-radius: 8px;
+        padding: 15px;
+        background-color: #${config.theme.colors.alt-bg};
+        color: #${config.theme.colors.alt-fg};
+      }
 
-            tooltip label {
-              padding: 5px;
-              background-color: #${config.theme.colors.alt-bg};
-              color: #${config.theme.colors.alt-fg};
-            }
+      tooltip label {
+        padding: 5px;
+        background-color: #${config.theme.colors.alt-bg};
+        color: #${config.theme.colors.alt-fg};
+      }
     '';
   };
 }
