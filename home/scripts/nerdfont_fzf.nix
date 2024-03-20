011c084203fc1ac97e29bc59563b578d5af27204
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-03-20 23:26:53 +0100

    I hate wl clipboard
    Former-commit-id: 24a84386942dc72253855a972256dbd6c2cd3ee8

--- home/scripts/nerdfont_fzf.nix
+++ home/scripts/nerdfont_fzf.nix
@@ -5,10 +5,15 @@ let
   nerdfont-fzf = pkgs.writeShellScriptBin "nerdfont-fzf" ''
     # Dependencies: jq, fzf, wl-copy, wget
     icons=$(jq -r 'to_entries[] | "\(.key):\(.value.char)"' "${homedir}/.config/nerdfont_glyphnames.json" | awk -F: '{print "\033[95m "$2" \033[0m "$1}')
-    fzf_result=$(echo "$icons" | fzf --ansi | awk '{print $1}' || exit 1)
+    fzf_result=$(echo "$icons" | fzf --ansi --border none | awk '{print $1}')
+    if [ -z "$fzf_result" ]; then
+      echo "No icon selected"
+      exit 1
+    fi
 
     echo "Copied to clipboard: $fzf_result"
-    printf "$fzf_result" | wl-copy 
+    echo "Paste it to close the window"
+    ${pkgs.wl-clipboard}/bin/wl-copy -o "$fzf_result" # wait for paste before exiting.. not cool
   '';
 
   nerdfont-fzf-fetch = pkgs.writeShellScriptBin "nerdfont-fzf-fetch" ''
@@ -16,8 +21,12 @@ let
     jq 'del(.METADATA)' "glyphnames.json"
   '';
 
+  nerdfont-popup = pkgs.writeShellScriptBin "nerdfont-popup" ''
+    ${pkgs.kitty}/bin/kitty --class floating -o font_size=18 zsh -c nerdfont-fzf 2>/dev/null
+  '';
+
 in {
-  home.packages = with pkgs; [ nerdfont-fzf nerdfont-fzf-fetch ];
+  home.packages = with pkgs; [ nerdfont-fzf nerdfont-fzf-fetch nerdfont-popup ];
 
   xdg.configFile."nerdfont_glyphnames.json" = {
     source = ../misc/nerdfont_glyphnames.json;
