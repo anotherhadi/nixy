9bd0c2f190c03785ef763bd3403bc06680bd51e2
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-03-20 18:31:59 +0100

    Themes
    Former-commit-id: 5ad31e76026fdd0ee60deb3f639138e9f6da3912

--- home/qutebrowser/duckduckgo-colorscheme.nix
+++ home/qutebrowser/duckduckgo-colorscheme.nix
@@ -1,6 +1,8 @@
 { config, ... }: {
 
   xdg.configFile."duckduckgo-colorscheme.js".text = ''
+    // Go to DuckDuckGo settings page, open the console, paste the code and hit enter.
+    // based on https://ddg.codingcodax.dev/
     // Cookies string for your theme
     const cookie = '7=${config.theme.colors.bg}; j=${config.theme.colors.bg}; 9=${config.theme.colors.fg}; aa=${config.theme.colors.alt-fg}; 8=${config.theme.colors.color7}; x=${config.theme.colors.primary-bg}; 21=${config.theme.colors.alt-bg};';
 
