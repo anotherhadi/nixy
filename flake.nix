2c910092c8ce70dcc58011f608c9afbe29c9269a
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-05-09 10:54:19 +0200

    Update
    Former-commit-id: a26fbaa2c3e139d74a360b9402d43911a7e2560c

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
   };
 
