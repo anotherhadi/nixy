cfbf65a7f5dec52c87c87e5c561e23a5231cbb28
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-05-17 14:52:18 +0200

    Add hyprland cachix
    Former-commit-id: 98186715b1823186cce658b7731d62c7f5c39106

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -122,6 +122,12 @@ in {
   } else
     { };
 
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys =
+      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
+
   services.dbus.enable = true;
 
   system.stateVersion = variable.stateVersion;
