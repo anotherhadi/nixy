65ec0240285029ac7fac784502712f1fa07a040a
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-07 22:25:21 +0200

    Update

--- hosts/server/nextcloud.nix
+++ hosts/server/nextcloud.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }: {
+
+  environment.systemPackages = with pkgs; [ nextcloud29 ];
+
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud28;
+    package = pkgs.nextcloud29;
     hostName = "localhost";
     config.adminpassFile = "/etc/nextcloudpwd";
   };
