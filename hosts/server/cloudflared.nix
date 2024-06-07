ebbb2dd780216d186274ce9ba092f0450da6946e
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-08 00:01:20 +0200

    Update
    Former-commit-id: 92b467525d999b1588c4b5f389c4f25d98924ede

--- hosts/server/cloudflared.nix
+++ hosts/server/cloudflared.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:{
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [ cloudflared ];
   services.cloudflared = {
     enable = true;
@@ -6,9 +6,7 @@
       "55ac0504-4ba0-4ea7-bf38-6d04ca7e45c3" = {
         credentialsFile = "/etc/cloudflaredpwd";
         default = "http_status:404";
-        ingress = {
-          "cloud.anotherhadi.com" = { service = "http://localhost:8080"; };
-        };
+        ingress = { };
       };
     };
   };
