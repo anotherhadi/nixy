89c194d6042bf050bb6030f9118c7c94570eda09
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-07 22:53:04 +0200

    Update
    Former-commit-id: 24de1041397c8d99eedc8faa3ce03d07317c615b

--- hosts/server/nextcloud.nix
+++ hosts/server/nextcloud.nix
@@ -7,8 +7,27 @@
     package = pkgs.nextcloud29;
     hostName = "localhost";
     config.adminpassFile = "/etc/nextcloudpwd";
-    settings = { overwritehost = "localhost:8080"; };
+    nginx.enable = true;
   };
 
-  networking.firewall.allowedTCPPorts = [ 8080 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  services.nginx = {
+    enable = true;
+
+    # Use recommended settings
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    # Only allow PFS-enabled ciphers with AES256
+    sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+
+    # Setup Nextcloud virtual host to listen on ports
+    virtualHosts = {
+
+      "localhost" = { };
+    };
+  };
 }
