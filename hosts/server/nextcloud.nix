7a3787d62d18d5f01821baffbdf8ad33b4acf8ce
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-08 02:26:47 +0200

    Update
    Former-commit-id: 4556c5ea2f0d775d9c72203fde501c70351b76b5

--- hosts/server/nextcloud.nix
+++ hosts/server/nextcloud.nix
@@ -9,23 +9,8 @@
     config.adminpassFile = "/etc/nextcloudpwd";
   };
 
-  networking.firewall.allowedTCPPorts = [ 80 443 8083 8080 ];
-
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
-    forceSSL = false;
-    enableACME = false;
-    listen = [{
-      addr = "localhost";
-      port = 8083;
-    }];
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:8080";
-      extraConfig = ''
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-      '';
-    };
+    forceSSL = true;
+    enableACME = true;
   };
 }
