0aae361973e9558465aee1d6a696e30f421cdc64
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-08 02:17:50 +0200

    Update
    Former-commit-id: 562dad765bd67fdc22e04b482117f3757aac6d4a

--- hosts/server/cloudflared.nix
+++ hosts/server/cloudflared.nix
@@ -1,46 +1,12 @@
-# { pkgs, ... }: {
-#   environment.systemPackages = with pkgs; [ cloudflared ];
-#   services.cloudflared = {
-#     enable = true;
-#     tunnels = {
-#       "55ac0504-4ba0-4ea7-bf38-6d04ca7e45c3" = {
-#         credentialsFile = "/etc/cloudflaredpwd";
-#         default = "http_status:404";
-#         ingress = { };
-#       };
-#     };
-#   };
-# }
-#
-{ config, pkgs, ... }:
-
-{
-  services.nextcloud = {
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [ cloudflared ];
+  services.cloudflared = {
     enable = true;
-    hostName = "localhost";
-    https = false;
-    config = {
-      dbtype = "sqlite";
-      adminuser = "admin";
-      adminpass = "TESTADMINPASSWORD";
-      datadirectory = "/var/lib/nextcloud/data";
-    };
-  };
-
-  services.postgresql.enable = true;
-
-  services.nginx = {
-    virtualHosts."localhost" = {
-      forceSSL = true;
-      enableACME = false;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8080";
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-        '';
+    tunnels = {
+      "55ac0504-4ba0-4ea7-bf38-6d04ca7e45c3" = {
+        credentialsFile = "/etc/cloudflaredpwd";
+        default = "http_status:404";
+        ingress = { };
       };
     };
   };
