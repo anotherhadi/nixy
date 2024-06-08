b828545a42c9702dd39f60152c891dcbce31900d
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-08 02:22:10 +0200

    Update
    Former-commit-id: e33ab5a6e129e330d0ad4e41a43b9a027a844d82

--- hosts/server/nextcloud.nix
+++ hosts/server/nextcloud.nix
@@ -18,5 +18,14 @@
       addr = "localhost";
       port = 8083;
     }];
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:8080";
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+      '';
+    };
   };
 }
