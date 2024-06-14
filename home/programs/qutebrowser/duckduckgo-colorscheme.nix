c410d04e1a393d959ea5807c3592b88bd834f9a7
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-14 21:04:33 +0200

    Update

--- home/programs/qutebrowser/duckduckgo-colorscheme.nix
+++ home/programs/qutebrowser/duckduckgo-colorscheme.nix
@@ -1,6 +1,5 @@
 { config, ... }: {
-
-  xdg.configFile."duckduckgo-colorscheme.js".text = ''
+  home.file.".duckduckgo-colorscheme.js".text = ''
     // Go to DuckDuckGo settings page, open the console, paste the code and hit enter.
     // based on https://ddg.codingcodax.dev/
     // Cookies string for your theme
