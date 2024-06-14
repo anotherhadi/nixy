d9810c3c1fd30e7dcd5d2093e5016a7701288acc
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-14 20:58:52 +0200

    Update
    Former-commit-id: fad7c6e46dd9954340ac0b127a9e0297866a0173

--- home/system/hyprland/hypridle.nix
+++ home/system/hyprland/hypridle.nix
@@ -1,23 +1,21 @@
-{ config, pkgs, ... }: {
-  home.packages = with pkgs; [ hypridle ];
+{ pkgs, ... }: {
 
-  xdg.configFile."hypr/hypridle.conf".text = ''
-    general {
-        ignore_dbus_inhibit = false
-    }
-
-    # Screenlock
-    listener {
-        timeout = 600
-        on-timeout = ${pkgs.hyprlock}/bin/hyprlock
-        on-resume = ${pkgs.libnotify}/bin/notify-send "Welcome back ${config.home.username}!"
-    }
-
-    # Suspend (not working on my laptop)
-    #listener {
-    #    timeout = 660
-    #    on-timeout = systemctl suspend
-    #    on-resume = ${pkgs.libnotify}/bin/notify-send "Welcome back to your desktop!"
-    #}
-  '';
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = { ignore_dbus_inhibit = false; };
+      listener = [
+        {
+          timeout = 600;
+          on-timeout = pkgs.hyprlock + "/bin/hyprlock";
+        }
+        {
+          timeout = 660;
+          on-timeout = "systemctl suspend";
+          on-resume = pkgs.libnotify
+            + "/bin/notify-send 'Welcome back to your desktop!'";
+        }
+      ];
+    };
+  };
 }
