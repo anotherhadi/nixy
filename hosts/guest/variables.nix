58bbb5532aa21dd2f72228c792af2367a0261568
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-17 14:18:15 +0200

    Add themes folder
    Former-commit-id: a6cc46cf93e4dd96dc8b71ba8ee319b6dfef4e56

--- hosts/guest/variables.nix
+++ hosts/guest/variables.nix
@@ -24,56 +24,6 @@
     sops = false;
     nextcloud = false;
 
-    theme = {
-      wallpaper = "lofi-computer.png";
-
-      font-mono = "FiraCode Nerd Font Mono";
-      font = "SFProDisplay Nerd Font";
-
-      rounding = 15;
-      gaps-in = 12;
-      gaps-out = config.var.theme.gaps-in * 2;
-      border-size = 4;
-      animation-speed = "slow"; # "fast" | "medium" | "slow"
-
-      fetch = "nerdfetch"; # "nerdfetch" | "neofetch" | "none"
-
-      waybar = {
-        transparent = true;
-        float = true;
-        position = "top";
-      };
-
-      colors = {
-        c0 = "2F2F2F"; # black
-        c1 = "f7768e"; # red
-        c2 = "73daca"; # green
-        c3 = "e0af68"; # yellow
-        c4 = "4973CB"; # blue
-        c5 = "A594FD"; # magenta
-        c6 = "7dcfff"; # cyan
-        c7 = "FFFFFF"; # white
-        c8 = "3F3F3F"; # bright black
-        c9 = "f7869e"; # bright red
-        c10 = "83eada"; # bright green
-        c11 = "f0bf78"; # bright yellow
-        c12 = "8ab2f7"; # bright blue
-        c13 = "A988F8"; # bright magenta
-        c14 = "8ddfff"; # bright cyan
-        c15 = "f4f5f7"; # bright white
-
-        bg = "040404";
-        fg = config.var.theme.colors.c7;
-        bgalt = "131313";
-        fgalt = config.var.theme.colors.c15;
-
-        accent = config.var.theme.colors.c5;
-        accentFg = config.var.theme.colors.c0;
-
-        # Should make those automtic
-        accentName = "magenta";
-        accentNumber = "5";
-      };
-    };
+    theme = import ../themes/nixy.nix;
   };
 }
---
+++ hosts/themes/nixy.nix
@@ -0,0 +1,50 @@
+{
+  wallpaper = "lofi-computer.png";
+
+  font-mono = "FiraCode Nerd Font Mono";
+  font = "SFProDisplay Nerd Font";
+
+  rounding = 15;
+  gaps-in = 12;
+  gaps-out = 12 * 2;
+  border-size = 4;
+  animation-speed = "slow"; # "fast" | "medium" | "slow"
+  fetch = "nerdfetch"; # "nerdfetch" | "neofetch" | "none"
+
+  waybar = {
+    transparent = true;
+    float = true;
+    position = "top";
+  };
+
+  colors = {
+    c0 = "2F2F2F"; # black
+    c1 = "f7768e"; # red
+    c2 = "73daca"; # green
+    c3 = "e0af68"; # yellow
+    c4 = "4973CB"; # blue
+    c5 = "A594FD"; # magenta
+    c6 = "7dcfff"; # cyan
+    c7 = "FFFFFF"; # white
+    c8 = "3F3F3F"; # bright black
+    c9 = "f7869e"; # bright red
+    c10 = "83eada"; # bright green
+    c11 = "f0bf78"; # bright yellow
+    c12 = "8ab2f7"; # bright blue
+    c13 = "A988F8"; # bright magenta
+    c14 = "8ddfff"; # bright cyan
+    c15 = "f4f5f7"; # bright white
+
+    bg = "040404";
+    fg = "FFFFFF";
+    bgalt = "131313";
+    fgalt = "f4f5f7";
+
+    accent = "A594FD";
+    accentFg = "2F2F2F";
+
+    # Should make those automtic
+    accentName = "magenta";
+    accentNumber = "5";
+  };
+}
