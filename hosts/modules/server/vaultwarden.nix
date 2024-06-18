a2485b18b36f7fc85bf8dfc9f77a4c22829efbda
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-18 10:10:21 +0200

    temp
    Former-commit-id: 93b7145e70adee2ed94e2864aa2595a40c2b0490

--- hosts/modules/server/vaultwarden.nix
+++ hosts/modules/server/vaultwarden.nix
@@ -1,9 +1,24 @@
 let
-
+    hostname = "vault.anotherhadi.com";
+    url = "https://"+hostname;
+    port = 8222;
 in{
     services.vaultwarden = {
         enable = true;
-        config = ''
-        
-    }
+        config = {
+            DOMAIN = url;
+            SIGNUPS_ALLOWED = false;
+            ROCKET_ADDRESS = "127.0.0.1";
+            ROCKET_PORT = port;
+            ROCKET_LOG = "critical";
+        };
+    };
+    services.nginx.virtualHosts.${hostname} = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+            proxyPass = "http://127.0.0.1:${port}";
+        };
+    };
+  networking.firewall.allowedTCPPorts = [ port ];
 }
