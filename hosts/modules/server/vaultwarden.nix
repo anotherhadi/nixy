c852176e0d84b5c805d435181679a22b5f721852
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 23:28:13 +0200

    Update

--- hosts/modules/server/vaultwarden.nix
+++ hosts/modules/server/vaultwarden.nix
@@ -1,25 +1,22 @@
 # Password manager
 let
-    hostname = "vault.anotherhadi.com";
-    url = "https://"+hostname;
-    port = 8222;
-in{
-    services.vaultwarden = {
-        enable = true;
-        config = {
-            DOMAIN = url;
-            SIGNUPS_ALLOWED = false;
-            ROCKET_ADDRESS = "127.0.0.1";
-            ROCKET_PORT = port;
-            ROCKET_LOG = "critical";
-        };
+  hostname = "vault.anotherhadi.com";
+  url = "https://" + hostname;
+  port = 8222;
+in {
+  services.vaultwarden = {
+    enable = true;
+    config = {
+      DOMAIN = url;
+      SIGNUPS_ALLOWED = true;
+      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_PORT = port;
+      ROCKET_LOG = "critical";
     };
-    services.nginx.virtualHosts.${hostname} = {
-        enableACME = true;
-        forceSSL = true;
-        locations."/" = {
-            proxyPass = "http://127.0.0.1:${port}";
-        };
-    };
+  };
+  services.nginx.virtualHosts.${hostname} = {
+    enableACME = true;
+    locations."/" = { proxyPass = "http://127.0.0.1:${port}"; };
+  };
   networking.firewall.allowedTCPPorts = [ port ];
 }
