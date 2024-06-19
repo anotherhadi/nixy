53ae7625afbcfb969740d28e0f648ec16e502f55
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 21:17:54 +0200

    test
    Former-commit-id: 530e9d8beaf6634e565510c4c12204b0f09139b9

--- home/system/sops/server.nix
+++ home/system/sops/server.nix
@@ -12,17 +12,14 @@
       nextcloud-adminpass = {
         path = "/etc/nextcloud/adminpassFile";
         owner = "nextcloud";
-        group = "nextcloud";
       };
       cloudflare-apitoken = {
         path = "/etc/cloudflare/apiToken";
         owner = "cloudflare-dyndns";
-        group = "cloudflare-dyndns";
       };
       tailscaled-authKey = {
         path = "/etc/tailscale/authKey";
         owner = "tailscaled";
-        group = "tailscaled";
       };
     };
   };
