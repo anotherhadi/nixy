178aeb2aac7d0347f998ab5ce5199b1117d4eef1
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 23:39:30 +0200

    Update
    Former-commit-id: adecf1bc29b1f0c605a9b3bd1b03d4fcff73187c

--- hosts/modules/server/tailscale.nix
+++ hosts/modules/server/tailscale.nix
@@ -1,14 +1,13 @@
-{config, ...}:{
+{
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "both";
+    authKeyFile = "/etc/tailscale/authKey";
+    openFirewall = true;
+    extraUpFlags = [ "--advertise-exit-node" ];
+  };
 
-    services.tailscale = {
-        enable = true;
-        useRoutingFeatures = "both";
-        authKeyFile = "/etc/tailscale/authKey";
-        openFirewall = true;
-        extraUpFlags = [ "--advertise-exit-node" ];
-    };
-
-    # Fix DNS & IPv6 bugs
-    networking.nftables.enable = true;
-    services.resolved.enable = true;
+  # Fix DNS & IPv6 bugs
+  networking.nftables.enable = true;
+  services.resolved.enable = true;
 }
