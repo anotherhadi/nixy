b5fe18ab2f6cf66380c5c89683e3f86328a2516b
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 21:41:04 +0200

    test

---
+++ hosts/modules/server/sops.nix
@@ -0,0 +1,47 @@
+{ inputs, ... }: {
+
+  imports = [ inputs.sops-nix.nixosModule ];
+
+  sops = {
+    age.keyFile = "/home/hadi/.config/sops/age/keys.txt";
+    defaultSopsFile = "../../../secrets/server.yaml";
+    secrets = {
+      sshconfig = {
+        path = "/home/hadi/.ssh/config";
+        owner = "hadi";
+      };
+      github-key = {
+        path = "/home/hadi/.ssh/github";
+        owner = "hadi";
+      };
+      cloudflare-apitoken = {
+        path = "/home/hadi/test.txt";
+        owner = "hadi";
+      };
+    };
+  };
+}
+
+# sops = {
+#   age.keyFile = "/home/hadi/.config/sops/age/keys.txt";
+#   defaultSopsFile = ../../../secrets/server.yaml;
+#   secrets = {
+#     sshconfig = { path = "/home/hadi/.ssh/config"; };
+#     github-key = { path = "/home/hadi/.ssh/github"; };
+# nextcloud-adminpass = {
+#   path = "/etc/nextcloud/adminpassFile";
+#   owner = "nextcloud";
+#   group = "nextcloud";
+# };
+# cloudflare-apitoken = {
+#   path = "/etc/cloudflare/apiToken";
+#   owner = "cloudflare-dyndns";
+#   group = "cloudflare-dyndns";
+# };
+# tailscaled-authKey = {
+#   path = "/etc/tailscale/authKey";
+#   owner = "tailscaled";
+#   group = "tailscaled";
+# };
+# };
+# };
