ff555046161a7d797bf0c2752eac422ace97cd2b
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 23:51:36 +0200

    Update
    Former-commit-id: 325f8e9d1546b36c525543d8972e56892c4f6eda

--- hosts/modules/server/ntfy-sh.nix
+++ hosts/modules/server/ntfy-sh.nix
@@ -2,8 +2,8 @@
 # https://mynixos.com/nixpkgs/options/services.ntfy-sh
 let
   hostname = "ntfy.anotherhadi.com";
-  url = "https://" + hostname;
   port = 8082;
+  url = "https://" + hostname + ":" + port;
 in {
   services.ntfy-sh = {
     enable = true;
@@ -17,7 +17,6 @@ in {
   };
   services.nginx.virtualHosts.${hostname} = {
     enableACME = true;
-    forceSSL = true;
     locations."/" = { proxyPass = "http://127.0.0.1:${toString port}"; };
   };
   networking.firewall.allowedTCPPorts = [ port ];
