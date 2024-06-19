debb139aeb517b8ea814011bacd2d1d67f02985b
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 23:20:49 +0200

    Update

--- hosts/modules/server/nextcloud.nix
+++ hosts/modules/server/nextcloud.nix
@@ -1,24 +1,22 @@
 # Nextcloud
-{ pkgs, ... }:
-let
-    hostname = "cloud.anotherhadi.com";
-in
-{
+let hostname = "cloud.anotherhadi.com";
+in {
   services.nextcloud = {
     enable = true;
     hostName = hostname;
     https = true;
     config = {
-        adminUser = "jack";
-        adminpassFile = "/etc/nextcloud/adminpassFile";
+      adminUser = "jack";
+      adminpassFile = "/etc/nextcloud/adminpassFile";
     };
     settings = {
-        trusted_domains = [ "localhost" "127.0.0.1" "192.168.2.23" "cloud.anotherhadi.com" ];
+      trusted_domains =
+        [ "localhost" "127.0.0.1" "192.168.2.23" "cloud.anotherhadi.com" ];
     };
     nginx.recommendedHttpHeaders = true;
   };
   services.nginx.virtualHosts.${hostname} = {
     forceSSL = true;
     enableACME = true;
-  }
+  };
 }
