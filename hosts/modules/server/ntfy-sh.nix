cafce8e7c9b52b0d60611ea588bc16594be3b155
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 23:47:35 +0200

    Update

--- hosts/modules/server/ntfy-sh.nix
+++ hosts/modules/server/ntfy-sh.nix
@@ -1,26 +1,24 @@
 # Push Notification Server
 # https://mynixos.com/nixpkgs/options/services.ntfy-sh
 let
-    hostname = "ntfy.anotherhadi.com";
-    url = "https://"+hostname;
-    port = 8082;
-in{
-    services.ntfy-sh = {
-        enable = true;
-        settings = {
-            base-url = url;
-            listen-http = "${port}";
-            # auth-file = "/etc/ntfy-sh/authfile"
-            auth-default-access = "deny-all";
-            behind-proxy = true;
-        };
+  hostname = "ntfy.anotherhadi.com";
+  url = "https://" + hostname;
+  port = 8082;
+in {
+  services.ntfy-sh = {
+    enable = true;
+    settings = {
+      base-url = url;
+      listen-http = "${port}";
+      # auth-file = "/etc/ntfy-sh/authfile"
+      auth-default-access = "deny-all";
+      behind-proxy = true;
     };
-    services.nginx.virtualHosts.${hostname} = {
-        enableACME = true;
-        forceSSL = true;
-        locations."/" = {
-            proxyPass = "http://127.0.0.1:${port}";
-        };
-    };
-    networking.firewall.allowedTCPPorts = [ port ];
+  };
+  services.nginx.virtualHosts.${hostname} = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = { proxyPass = "http://127.0.0.1:${port}"; };
+  };
+  networking.firewall.allowedTCPPorts = [ port ];
 }
