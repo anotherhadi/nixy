4c0a61ccada918ee9899772654c48ad8e2a0ddb6
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 21:01:01 +0200

    Update
    Former-commit-id: 5414f496716af414effa08c7ae891e2a36dfc5b3

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -11,5 +11,6 @@
     ./system
     ./compress
     ./misc
+    ./tofi
   ];
 }
--- home/scripts/nixy/default.nix
+++ home/scripts/nixy/default.nix
@@ -1,6 +1,55 @@
 { pkgs, config, ... }:
 
 let
+
+  nixy = pkgs.writeShellScriptBin "nixy" ''
+    function exec() {
+      $@
+      # hyprctl dispatch exec $@
+    }
+
+    function ui(){
+      DEFAULT_ICON="󰘳"
+
+      # "icon;name;command"[]
+      apps=(
+        ";Rebuild;nixy-rebuild"
+        ";Upgrade;nixy-upgrade"
+        ";Update;nixy-update"
+        ";Collect Garbage;nixy-gc"
+        ";Clean Boot Menu;nixy-cb"
+      )
+
+      # Apply default icons if empty:
+      for i in "''${!apps[@]}"; do
+        apps[i]=$(echo "''${apps[i]}" | sed 's/^;/'$DEFAULT_ICON';/')
+      done
+
+      fzf_result=$(printf "%s\n" "''${apps[@]}" | awk -F ';' '{print $1" "$2}' | fzf)
+      [[ -z $fzf_result ]] && exit 0
+      fzf_result=''${fzf_result/ /;}
+      line=$(printf "%s\n" "''${apps[@]}" | grep "$fzf_result")
+      command=$(echo "$line" | sed 's/^[^;]*;//;s/^[^;]*;//')
+
+      exec "$command"
+      exit 0
+    }
+
+    [[ $1 == "" ]] && ui
+
+    if [[ $1 == "rebuild" ]];then
+      nixy-rebuild
+    elif [[ $1 == "upgrade" ]];then
+      nixy-upgrade
+    elif [[ $1 == "update" ]];then
+      nixy-update
+    elif [[ $1 == "gc" ]];then
+      nixy-gc
+    elif [[ $1 == "cb" ]];then
+      nixy-cb
+    fi
+  '';
+
   nixy-rebuild = pkgs.writeShellScriptBin "nixy-rebuild" ''
     sudo nixos-rebuild switch --flake ${config.var.configDirectory}#nixy
   '';
@@ -23,6 +72,7 @@ let
 
 in {
   home.packages = with pkgs; [
+    nixy
     nixy-rebuild
     nixy-upgrade
     nixy-update
