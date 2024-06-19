e51b20e4ab7dd435bffc466c1043900e60a2e6b6
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 23:07:47 +0200

    Update

--- hosts/modules/server/nginx.nix
+++ hosts/modules/server/nginx.nix
@@ -1,9 +1,16 @@
 # Nginx Reverse Proxy & ACME certs
-{config, ...}:{
+{ config, ... }: {
   services.nginx.enable = true;
   security.acme = {
     acceptTerms = true;
-    defaults.email = ${config.var.git.email};
+    defaults.email = config.var.git.email;
   };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  # test
+  services.nginx.virtualHosts."anotherhadi.com" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/".root = "/var/www/";
+  };
 }
