cd08d5ad469dc4ee445a36a09c1e2e481b244116
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 22:46:33 +0200

    Update
    Former-commit-id: 241a91487cf22638f0c48288b486d83176a4d9da

--- hosts/modules/server/sops.nix
+++ hosts/modules/server/sops.nix
@@ -1,7 +1,5 @@
 { inputs, ... }: {
 
-  imports = [ inputs.sops-nix.nixosModules.sops ];
-
   sops = {
     age.keyFile = "/home/hadi/.config/sops/age/keys.txt";
     defaultSopsFile = "../../../secrets/server.yaml";
