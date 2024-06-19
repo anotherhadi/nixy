c0b0ed81c8c946997607b08aaa29ee75b3155e98
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-20 01:31:30 +0200

    Update
    Former-commit-id: 1b4cd64024dbcbfe04befc84d81525efe3eb2003

--- hosts/modules/server/www/default.nix
+++ hosts/modules/server/www/default.nix
@@ -4,10 +4,11 @@
 
   services.nginx.virtualHosts = {
     "anotherhadi.com" = {
-      serverAliases = [ "www.anotherhadi.com" ];
+      # serverAliases = [ "www.anotherhadi.com" ];
       root = "/etc/www/anotherhadi.com";
     };
     "home.anotherhadi.com" = { root = "/etc/www/home.anotherhadi.com"; };
+    "www.anotherhadi.com" = { root = "/var/www/test"; };
   };
   environment.etc."www/anotherhadi.com" = { source = ./anotherhadi.com; };
 }
