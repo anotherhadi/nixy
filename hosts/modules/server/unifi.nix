44b79b0ff0c9fe39229412af0d8d14731a99f244
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-19 23:45:03 +0200

    Update
    Former-commit-id: 21482d4a444444d610d4c6f9964b5cf0edf583e1

--- hosts/modules/server/unifi.nix
+++ hosts/modules/server/unifi.nix
@@ -1,7 +1,7 @@
 # unifi controller service
 {
-    services.unifi = {
-        enable = true;
-        openFirewall = true; # 8443(webui) and other port use by AP
-    };
+  services.unifi = {
+    enable = true;
+    openFirewall = true; # 8443(webui) and other port use by AP
+  };
 }
