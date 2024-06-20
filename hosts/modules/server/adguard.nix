34a090cb13f6c3f7b1b38a6e7fabf54b30dc6b67
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-20 16:32:27 +0200

    Update
    Former-commit-id: 9e191c76eb1cfeecda25359a3c01863337475f5a

--- hosts/modules/server/adguard.nix
+++ hosts/modules/server/adguard.nix
@@ -2,9 +2,11 @@
 # Change your router primary DNS server to {this server IP} to make it network wide.
 {
   services.adguardhome = {
-    enable = false;
+    enable = true;
     host = "0.0.0.0";
     port = 3000;
+    allowDHCP = true;
+    openFirewall = true;
   };
   networking.firewall.allowedTCPPorts = [ 53 3000 ];
 }
