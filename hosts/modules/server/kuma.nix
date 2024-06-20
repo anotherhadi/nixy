76a423a964c1e4ccb33f80cd5180dee1baf5f453
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-20 11:31:02 +0200

    Update
    Former-commit-id: e0803c6bd54efbcbc0e4eccac4fdc753155b3c6b

--- hosts/modules/server/kuma.nix
+++ hosts/modules/server/kuma.nix
@@ -6,5 +6,9 @@
       PORT = "3005";
     };
   };
+  services.nginx.virtualHosts."kuma.anotherhadi.com" = {
+    enableACME = true;
+    locations."/" = { proxyPass = "http://127.0.0.1:3005"; };
+  };
   networking.firewall.allowedTCPPorts = [ 3005 ];
 }
