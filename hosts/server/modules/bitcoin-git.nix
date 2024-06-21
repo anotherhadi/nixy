9ab4dae87b9a3e8fb8d888721d371178a62ea60e
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-21 13:58:14 +0200

    Update
    Former-commit-id: 154c21859d98578b3387225c50c43785d7f12599

--- hosts/server/modules/bitcoin-git.nix
+++ hosts/server/modules/bitcoin-git.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   systemd.timers."bitcoin-git" = {
     wantedBy = [ "timers.target" ];
     timerConfig = {
@@ -10,17 +10,17 @@
 
   systemd.services."bitcoin-git" = {
     script = ''
-      [ ! -d "/home/hadi/btcprice" ] && git clone git@github.com:anotherhadi/btcprice /home/hadi/btcprice
+      [ ! -d "/home/hadi/btcprice" ] && ${pkgs.git}/bin/git clone git@github.com:anotherhadi/btcprice /home/hadi/btcprice
       cd "/home/hadi/btcprice"
-      price=$(curl eur.rate.sx/1BTC)
+      price=$(${pkgs.curl}/bin/curl eur.rate.sx/1BTC)
       echo "# Bitcoin Price in EURO" >README.md
       echo "" >>README.md
       echo "1 Bitcoin = $price€" >>README.md
       echo "" >>README.md
       echo "Date: $(date)" >>README.md
-      git add .
-      git commit -m "Update price (hadi)"
-      git push -u origin main
+      ${pkgs.git}/bin/git add .
+      ${pkgs.git}/bin/git commit -m "Update price (hadi)"
+      ${pkgs.git}/bin/git push -u origin main
       exit 0
     '';
     serviceConfig = {
