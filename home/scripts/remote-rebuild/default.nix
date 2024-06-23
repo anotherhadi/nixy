d1f5aa6dfab2d7a3ecd6fa67fcbc09c48f62690d
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-23 13:53:01 +0200

    Add remote rebuild (server)

--- home/scripts/remote-rebuild/default.nix
+++ home/scripts/remote-rebuild/default.nix
@@ -3,7 +3,7 @@
 let
 
   remote-rebuild = pkgs.writeShellScriptBin "remote-rebuild" ''
-    ssh jack -C "cd /home/hadi/.config/nixos && git pull && nixy-rebuild"
+    ssh jack -S -C "cd /home/hadi/.config/nixos && git pull && sudo -S nixos-rebuild switch --flake ~/.config/nixos#jack"
   '';
 
 in { home.packages = with pkgs; [ remote-rebuild ]; }
