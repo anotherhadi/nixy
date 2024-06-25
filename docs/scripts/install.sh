4e5cdbbac5f579ef5b61e296e2754445f76aac7b
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-25 09:49:11 +0200

    up

--- docs/scripts/install.sh
+++ docs/scripts/install.sh
@@ -1,10 +1,12 @@
-# installation script...
+#!/usr/bin/env bash
 
-# check if on nixos
-# check if ./hosts exist
-# check if gum is installed (dependencie)
-# save path
-# remove .git
-# git init and initial commit
-# create custom configuration
-# save it to ./hosts
+# https://github.com/anotherhadi/nixy
+# Installation script: Should be run on fresh nixos install.
+
+printf "\n\e[0;92m- \e[0m\e[1;77mNixy's Installation Script\e[0;96m [anotherhadi/nixy]\e[0m"
+
+# Check if we're on the good directory
+[[ -f "./docs/scripts/install.sh" ]] || (printf "\n\e[0;91mx \e[0m\e[1;77mNot in the project root directory\e[0m"; exit 1)
+DIR=$(pwd)
+
+# Create a new configuration
