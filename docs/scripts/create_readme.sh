ff1d70ba794f4a8eb4b0efb2444637af212c5592
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-26 14:46:22 +0000

    add md-table-of-contents source
    Former-commit-id: 6a8382c79edfbab5c238a9eeb451e4a2416e8a40

--- docs/scripts/create_readme.sh
+++ docs/scripts/create_readme.sh
@@ -7,7 +7,7 @@
 README_FILE="./README.md"
 
 header=$(cat "./docs/src/header.md")
-table_of_content=$(md-table-of-contents ./docs/src/README_template.md)
+table_of_content=$(md-table-of-contents ./docs/src/README_template.md) # https://github.com/anotherhadi/md-table-of-contents
 version=$(git describe --tags --abbrev=0)
 readme_content=$(cat "./docs/src/README_template.md")
 
