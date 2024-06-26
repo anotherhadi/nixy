1df0f31dbc41843daa11423fe3bf7d6fc72b3919
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-26 21:19:04 +0200

    Update

---
+++ home/programs/yazi/default.nix
@@ -0,0 +1,39 @@
+{
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+
+    settings = {
+      log = { enabled = false; };
+      manager = {
+        show_hidden = false;
+        sort_by = "modified";
+        sort_dir_first = true;
+        sort_reverse = true;
+      };
+    };
+
+    theme = {
+      filetype = {
+        rules = [
+          {
+            fg = "#7AD9E5";
+            mime = "image/*";
+          }
+          {
+            fg = "#F3D398";
+            mime = "video/*";
+          }
+          {
+            fg = "#F3D398";
+            mime = "audio/*";
+          }
+          {
+            fg = "#CD9EFC";
+            mime = "application/x-bzip";
+          }
+        ];
+      };
+    };
+  };
+}
