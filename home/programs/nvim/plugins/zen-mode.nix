af6319c8ec80798abe439334de334f32d7c57c16
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-28 14:35:35 +0200

    update
    Former-commit-id: 1f35859f2c6b23a3e2231bd438300db6e49f4ce2

--- home/programs/nvim/plugins/zen-mode.nix
+++ home/programs/nvim/plugins/zen-mode.nix
@@ -1,1 +1,30 @@
-{ programs.nixvim.plugins.zen-mode = { enable = true; }; }
+{
+  programs.nixvim.plugins.zen-mode = {
+    enable = true;
+    settings = {
+      on_open = ''
+        function()
+          require("gitsigns.actions").toggle_current_line_blame()
+          vim.opt.relativenumber = false
+          vim.opt.signcolumn = "no"
+        end
+      '';
+      plugins = {
+        gitsigns = { enabled = true; };
+        options = {
+          enabled = true;
+          ruler = false;
+          showcmd = false;
+        };
+        tmux = { enabled = false; };
+        twilight = { enabled = false; };
+      };
+      window = {
+        backdrop = 0.95;
+        height = 1;
+        options = { signcolumn = "no"; };
+        width = 0.8;
+      };
+    };
+  };
+}
