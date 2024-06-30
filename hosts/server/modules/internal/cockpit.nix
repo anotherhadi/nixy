cc2b0989d399742f456e893fe64c1a020c41afd3
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-06-30 17:42:09 +0200

    update
    Former-commit-id: c4d436b43493557b06ed72da94cf1add7ff05ff0

--- hosts/server/modules/internal/cockpit.nix
+++ hosts/server/modules/internal/cockpit.nix
@@ -1,8 +1,15 @@
 # Web-based graphical interface for servers
-{
+{ pkgs, ... }: {
   services.cockpit = {
     enable = true;
     port = 3009;
     openFirewall = true;
+    settings.WebService = { AllowUnencrypted = true; };
   };
+
+  environment.systemPackages = with pkgs; [
+    networkmanagerapplet
+    selinux-python
+    cockpit
+  ];
 }
