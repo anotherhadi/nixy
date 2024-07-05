c6fee1ccd6099ead53ce39124f0bb2018cb9d2db
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-07-06 00:35:41 +0200

    Update
    Former-commit-id: cd7db37ccd329f4683501ca16c7a5c5f64afff53

--- home/programs/nvim/keymaps.nix
+++ home/programs/nvim/keymaps.nix
@@ -1,14 +1,7 @@
 {
-  programs.nixvim.keymaps = [
-    {
-      key = "<space>fm";
-      action = "<cmd>lua vim.lsp.buf.format()<cr>";
-      options.desc = "LSP Format";
-    }
-    {
-      key = "<space>ft";
-      action = "vip:!pandoc -t commonmark_x<cr>";
-      options.desc = "Format markdown table";
-    }
-  ];
+  programs.nixvim.keymaps = [{
+    key = "<space>fm";
+    action = "<cmd>lua vim.lsp.buf.format()<cr>";
+    options.desc = "LSP Format";
+  }];
 }
