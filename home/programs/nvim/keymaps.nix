8e5df9ff90b34b5d15c974c3bb658ef3f1241dea
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-07-09 12:05:04 +0200

    Update
    Former-commit-id: 9142814f8f97f9752cb921cec69f15c5fd52c899

--- home/programs/nvim/keymaps.nix
+++ home/programs/nvim/keymaps.nix
@@ -1,7 +1,14 @@
 {
-  programs.nixvim.keymaps = [{
-    key = "<space>fm";
-    action = "<cmd>lua vim.lsp.buf.format()<cr>";
-    options.desc = "LSP Format";
-  }];
+  programs.nixvim.keymaps = [
+    {
+      key = "<leader>fm";
+      action = "<cmd>lua vim.lsp.buf.format()<cr>";
+      options.desc = "LSP Format";
+    }
+    {
+      key = "<leader>w";
+      action = "<cmd>set wrap!<cr>";
+      options.desc = "Toggle Word Wrapping";
+    }
+  ];
 }
