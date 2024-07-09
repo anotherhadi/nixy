af3b9fcd6e59624fcf1ad38827beebafa41b5baa
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-07-09 13:41:42 +0200

    Update
    Former-commit-id: 0509eba07fa1ec59eac87a5c834dc0ffcbf056dc

--- home/programs/nvim/keymaps.nix
+++ home/programs/nvim/keymaps.nix
@@ -8,7 +8,12 @@
     {
       key = "<leader>w";
       action = "<cmd>set wrap!<cr>";
-      options.desc = "Toggle Word Wrapping";
+      options.desc = "Toggle word wrapping";
+    }
+    {
+      key = "<leader>W";
+      action = "<cmd>set linebreak!<cr>";
+      options.desc = "Toggle linebreak";
     }
   ];
 }
