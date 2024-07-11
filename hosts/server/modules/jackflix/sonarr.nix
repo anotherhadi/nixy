b5f27c6ed6b6b09e521cf21c80273a0b588bc1c4
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-07-11 16:07:04 +0200

    Update
    Former-commit-id: 1dc5190c43fde94516e67394f06ad88f790e6ae9

--- hosts/server/modules/jackflix/sonarr.nix
+++ hosts/server/modules/jackflix/sonarr.nix
@@ -1,6 +1,9 @@
 {
   services.sonarr = {
     enable = true;
+    user = "jackflix";
+    group = "jackflix";
+    dataDir = "/home/jackflix/sonarr";
     openFirewall = true;
   };
 }
--- hosts/server/modules/jackflix/transmission.nix
+++ hosts/server/modules/jackflix/transmission.nix
@@ -1,22 +1,22 @@
 # I'm using the transmission-openvpn docker image to be sure that my torrent traffic is going through a VPN.
 # Only to download legal stuff of course.
 {
-  environment.etc."/etc/transmission.sh" = {
-    text = ''
-      docker run --cap-add=NET_ADMIN -d \
-        -v /var/lib/transmission-openvpn/data:/data \
-        -v /var/lib/transmission-openvpn/config:/config \
-        -e OPENVPN_PROVIDER=PIA \
-        -e OPENVPN_CONFIG=france \
-        -e OPENVPN_USERNAME=username\
-        -e OPENVPN_PASSWORD="password" \
-        -e LOCAL_NETWORK=192.168.2.0/24 \
-        --log-driver json-file \
-        --log-opt max-size=10m \
-        -p 9091:9091 \
-        haugene/transmission-openvpn
-    '';
-  };
+  # environment.etc."/etc/transmission.sh" = {
+  #   text = ''
+  #     docker run --cap-add=NET_ADMIN -d \
+  #       -v /var/lib/transmission-openvpn/data:/data \
+  #       -v /var/lib/transmission-openvpn/config:/config \
+  #       -e OPENVPN_PROVIDER=PIA \
+  #       -e OPENVPN_CONFIG=france \
+  #       -e OPENVPN_USERNAME=username\
+  #       -e OPENVPN_PASSWORD="password" \
+  #       -e LOCAL_NETWORK=192.168.2.0/24 \
+  #       --log-driver json-file \
+  #       --log-opt max-size=10m \
+  #       -p 9091:9091 \
+  #       haugene/transmission-openvpn
+  #   '';
+  # };
   # TODO: launch the container at boot
 
   networking.firewall.allowedTCPPorts = [ 9091 ];
