321914861f3bc208b96846e6432a34bf13165888
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-08-18 23:40:47 +0200

    Release v2.1.0
    Former-commit-id: a134abdea3a1919001a3e52aac6e1f5dcbc297c8

---
+++ docs/scripts/new_tag.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+[[ -d "./docs" ]] || (echo "Folder ./docs not found" && exit 1)
+
+TAG=$(git describe --tags --abbrev=0)
+echo "Current tag: $TAG"
+
+read -rp "Enter the new tag name: " NEW_TAG
+
+echo "Compresing png files..."
+bash ./docs/scripts/compress_png.sh
+echo "Creating wallpapers preview..."
+bash ./docs/scripts/create_preview_wallpapers.sh
+echo "Creating README.md..."
+bash ./docs/scripts/create_readme.sh
+echo "Creating keybindings.md..."
+bash ./docs/scripts/create_keybindings.sh
+
+git add .
+git commit -m "Release $NEW_TAG"
+git tag -a "$NEW_TAG" -m "Release $NEW_TAG"
+git push origin "$NEW_TAG"
