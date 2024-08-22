04015018a44afcedc1fcdd93db2d4b3bbb6278e5
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-08-22 16:11:48 +0200

    update
    Former-commit-id: 771dceeb068f048e13421e7da439ff9d1dd7cbb4

--- hosts/server/modules/jackflix/jellyfin.nix
+++ hosts/server/modules/jackflix/jellyfin.nix
@@ -3,7 +3,10 @@
     enable = true;
     user = "jackflix";
     group = "jackflix";
-    dataDir = "/home/jackflix/jellyfin";
+    dataDir = "/home/jackflix/jellyfin/data";
+    cacheDir = "/home/jackflix/jellyfin/cache";
+    logDir = "/home/jackflix/jellyfin/log";
+    configDir = "/home/jackflix/jellyfin/config";
     openFirewall = true;
   };
 }
