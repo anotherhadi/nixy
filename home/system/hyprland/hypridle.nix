fa63cf950ae0027968e4832ad380250f0bcbecb3
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-08-29 14:29:05 +0200

    Update
    Former-commit-id: 55a5c52c51c589f97637c1511c42499d6a7786db

--- home/system/hyprland/hypridle.nix
+++ home/system/hyprland/hypridle.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }: {
+  wayland.windowManager.hyprland.settings.exec-once =
+    [ "${pkgs.hypridle}/bin/hypridle" ];
 
   services.hypridle = {
     enable = true;
--- hosts/modules/network-manager.nix
+++ hosts/modules/network-manager.nix
@@ -1,6 +1,4 @@
 { pkgs, ... }: {
   networking.networkmanager.enable = true;
   systemd.services.NetworkManager-wait-online.enable = false;
-
-  environment.systemPackages = with pkgs; [ networkmanagerapplet ];
 }
