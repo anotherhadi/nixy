66e52892bade0ac350980274cc8f6e4abe8a72dd
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-08-30 16:06:22 +0200

    Update
    Former-commit-id: fcd5eca04f97319811afc727252d5e524c78bb5f

---
+++ hosts/themes/windows.nix
@@ -0,0 +1,52 @@
+{
+  wallpaper = "moutains_black.png";
+
+  font = "SFProDisplay Nerd Font";
+  font-mono = "FiraCode Nerd Font Mono";
+  font-size = 14;
+
+  rounding = 10;
+  gaps-in = 6;
+  gaps-out = 6 * 2;
+  border-size = 1;
+  animation-speed = "medium"; # "fast" | "medium" | "slow"
+  fetch = "none"; # "nerdfetch" | "neofetch" | "pfetch" | "none"
+
+  bar = {
+    transparent = false;
+    floating = false;
+    font-size = 16;
+  };
+
+  colors = {
+    c0 = "1e1e2e"; # black
+    c1 = "f38ba8"; # red
+    c2 = "a6e3a1"; # green
+    c3 = "fab387"; # yellow
+    c4 = "89b4fa"; # blue
+    c5 = "A594FD"; # magenta
+    c6 = "74c7ec"; # cyan
+    c7 = "cdd6f4"; # white
+    c8 = "313244"; # bright black
+    c9 = "eba0ac"; # bright red
+    c10 = "94e2d5"; # bright green
+    c11 = "f9e2af"; # bright yellow
+    c12 = "b4befe"; # bright blue
+    c13 = "f5c2e7"; # bright magenta
+    c14 = "94e2d5"; # bright cyan
+    c15 = "bac2de"; # bright white
+
+    bg = "0B0B1B";
+    fg = "EAF0F5";
+    bgalt = "121222";
+    fgalt = "EAF0F5";
+
+    accent = "74c7ec";
+    accentFg = "1E1E2E";
+    accentalt = "94e2d5";
+
+    # Should make those automtic
+    accentName = "cyan";
+    accentNumber = "6";
+  };
+}
