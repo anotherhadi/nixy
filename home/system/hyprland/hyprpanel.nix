9943a1dacf22f10cf0cab1bb80920449277aee1e
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-08-30 09:33:57 +0200

    Update
    Former-commit-id: b3f20e6aefd00c8bbb8511189a5e70c61fd29319

--- home/system/hyprland/hyprpanel.nix
+++ home/system/hyprland/hyprpanel.nix
@@ -2,7 +2,7 @@
   wayland.windowManager.hyprland.settings.exec-once =
     [ "${pkgs.hyprpanel}/bin/hyprpanel" ];
 
-  home.packages = with pkgs; [ hyprpanel ];
+  home.packages = with pkgs; [ hyprpanel libnotify ];
 
   home.file.".cache/ags/hyprpanel/options.json" = {
     text = # json
