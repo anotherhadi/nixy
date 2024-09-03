659509e613ef9ed16f7b6ed6c1d8a475e0120ae0
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-09-04 01:40:24 +0200

    Change to gammastep

--- home/scripts/notification/default.nix
+++ home/scripts/notification/default.nix
@@ -10,9 +10,10 @@ let
       description=$3
 
       [[ -d "$NOTIF_FOLDER" ]] || mkdir $NOTIF_FOLDER
-      [[ -f "$NOTIF_FOLDER/$sender_id" ]] || echo "0" > "$NOTIF_FOLDER/$sender_id"
+      [[ -f "$NOTIF_FOLDER/$sender_id" ]] || (echo "0" > "$NOTIF_FOLDER/$sender_id")
 
       old_notification_id=$(cat "$NOTIF_FOLDER/$sender_id")
+      [[ -z "$old_notification_id" ]] && old_notification_id=0
 
        ${pkgs.libnotify}/bin/notify-send \
       --replace-id="$old_notification_id" --print-id \
