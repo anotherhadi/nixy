4135134261f4ee0bb77536c84d5815ede9ca3f11
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-09-04 01:53:36 +0200

    Add wofi clipman

---
+++ home/system/clipman/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+let
+
+  clipboard-clear = pkgs.writeShellScriptBin "clipboard-clear" ''
+    clipman clear --all
+  '';
+
+  clipboard = pkgs.writeShellScriptBin "clipboard" ''
+    clipman pick --tool=wofi
+  '';
+
+in {
+  wayland.windowManager.hyprland.settings.exec-once =
+    [ "${clipboard-clear}" "wl-paste -t text --watch clipman store" ];
+  home.packages = with pkgs; [ clipman clipboard clipboard-clear ];
+  services.clipman.enable = true;
+
+}
