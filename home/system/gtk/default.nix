932f6b6dbbe5dac57cc2cae9bfbf03874e0979fc
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-09-04 21:04:53 +0200

    Update
    Former-commit-id: 0489f7eaa73b81f93a49f1c7a268e67817f11ac9

--- home/system/gtk/default.nix
+++ home/system/gtk/default.nix
@@ -20,6 +20,18 @@
       name = config.var.theme.font;
       size = config.var.theme.font-size;
     };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
   };
 
   home.pointerCursor = {
