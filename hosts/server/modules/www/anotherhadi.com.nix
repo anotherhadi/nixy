e6b74067304869d3f50e30a007b004b5e7a5bc27
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-09-04 21:07:53 +0200

    update
    Former-commit-id: da2aab6f762cf3950fc62b63e12d42d80bbb36e1

--- hosts/server/modules/www/anotherhadi.com.nix
+++ hosts/server/modules/www/anotherhadi.com.nix
@@ -2,6 +2,6 @@
   services.nginx.virtualHosts."anotherhadi.com" = {
     serverAliases = [ "www.anotherhadi.com" ];
     enableACME = true;
-    root = inputs.anotherhadi-portfolio.packages."${pkgs.system}";
+    root = inputs.anotherhadi-portfolio.packages."${pkgs.system}".default;
   };
 }
