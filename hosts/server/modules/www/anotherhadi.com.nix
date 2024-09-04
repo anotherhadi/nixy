9cb8036b73b2394781ec2610bfcb3318da0c3bd4
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-09-04 21:04:53 +0200

    Update
    Former-commit-id: 0489f7eaa73b81f93a49f1c7a268e67817f11ac9

--- hosts/server/modules/www/anotherhadi.com.nix
+++ hosts/server/modules/www/anotherhadi.com.nix
@@ -1,35 +1,7 @@
-{ pkgs, lib, ... }:
-let
-  package = pkgs.buildNpmPackage {
-    pname = "homepage";
-    version = "0.0.0";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "anotherhadi";
-      repo = "portfolio";
-      rev = "5d54a2d204cdbf0f702173c1bf1279bca25eb736";
-      hash = "sha256-8vDm9YwZKpMujILZjXtRERUIxvDqddSxS/gdJfZdUdY=";
-    };
-
-    npmDepsHash = "sha256-KMILWgZ6GNKoe/+RZS1tVDTd3SxXl6YtOuCb5kSFeis=";
-
-    buildPhase = ''
-      npm install
-      npm run build
-      mkdir $out
-      mv build $out
-    '';
-
-    meta = {
-      description = "portfolio";
-      homepage = "https://github.com/anotherhadi/portfolio";
-    };
-  };
-
-in {
+{ inputs, pkgs, ... }: {
   services.nginx.virtualHosts."anotherhadi.com" = {
     serverAliases = [ "www.anotherhadi.com" ];
     enableACME = true;
-    root = package + "/build";
+    root = inputs.anotherhadi-portfolio.packages."${pkgs.system}";
   };
 }
