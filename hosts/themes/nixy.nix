ac293a378c97de06439fb126f9c585e17b61bf6c
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-09-16 17:24:47 +0200

    Update

--- hosts/themes/nixy.nix
+++ hosts/themes/nixy.nix
@@ -1,5 +1,5 @@
 {
-  wallpaper = "nix.png";
+  wallpaper = "20.png";
 
   font = "SFProDisplay Nerd Font";
   font-mono = "FiraCode Nerd Font Mono";
@@ -8,8 +8,8 @@
   rounding = 15;
   gaps-in = 10;
   gaps-out = 10 * 2;
-  active-opacity = 0.95;
-  inactive-opacity = 0.9;
+  active-opacity = 1;
+  inactive-opacity = 0.89;
   blur = true;
   border-size = 3;
   animation-speed = "fast"; # "fast" | "medium" | "slow"
