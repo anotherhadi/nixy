ed045316fff0334168790367ea4a5bb67d5334d1
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-09-18 23:53:39 +0200

    Update
    Former-commit-id: e47edc0c1f167d9fd19ba3640985ced2ed75f28c

--- home/programs/nvim/keymaps.nix
+++ home/programs/nvim/keymaps.nix
@@ -5,20 +5,55 @@
       action = "<cmd>lua vim.lsp.buf.format()<cr>";
       options.desc = "LSP Format";
     }
+
+    # UI
     {
-      key = "<leader>w";
+      key = "<leader>uw";
       action = "<cmd>set wrap!<cr>";
       options.desc = "Toggle word wrapping";
     }
     {
-      key = "<leader>W";
+      key = "<leader>uW";
       action = "<cmd>set linebreak!<cr>";
       options.desc = "Toggle linebreak";
     }
+
     {
-      key = "<leader>z";
+      key = "<leader>uz";
       action = "<cmd>ZenMode<cr>";
-      options.desc = "ZenMode";
+      options.desc = "Toggle ZenMode";
+    }
+
+    # Trouble
+    {
+      key = "<leader>tx";
+      action = "<cmd>Trouble diagnostics toggle<cr>";
+      options.desc = "Diagnostics (Trouble)";
+    }
+    {
+      key = "<leader>tX";
+      action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+      options.desc = "Buffer Diagnostics (Trouble)";
+    }
+    {
+      key = "<leader>ts";
+      action = "<cmd>Trouble symbols toggle focus=false<cr>";
+      options.desc = "Symbols (Trouble)";
+    }
+    {
+      key = "<leader>tl";
+      action = "<cmd>Trouble lsp toggle focus=false win.position=right<cr>";
+      options.desc = "LSP Definitions / references / ... (Trouble)";
+    }
+    {
+      key = "<leader>tL";
+      action = "<cmd>Trouble loclist toggle<cr>";
+      options.desc = "Location List (Trouble)";
+    }
+    {
+      key = "<leader>tQ";
+      action = "<cmd>Trouble qflist toggle<cr>";
+      options.desc = "Quickfix List (Trouble)";
     }
   ];
 }
