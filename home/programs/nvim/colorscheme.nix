58fa41a2104e546f6a1c0cabb33feed2c2f708c4
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-10-04 21:03:01 +0200

    Update

--- home/programs/nvim/colorscheme.nix
+++ home/programs/nvim/colorscheme.nix
@@ -1,11 +1,46 @@
 {
   programs.nixvim = {
-    enable = true;
-    colorschemes.catppuccin = {
-      enable = true;
-      settings = {
-        transparent_background = true;
-        flavour = "mocha";
+    colorschemes = {
+      catppuccin = {
+        enable = true;
+        settings = {
+          background = {
+            light = "macchiato";
+            dark = "mocha";
+          };
+          flavour =
+            "mocha"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
+          disable_bold = false;
+          disable_italic = false;
+          disable_underline = false;
+          transparent_background = true;
+          term_colors = true;
+          integrations = {
+            cmp = true;
+            noice = true;
+            notify = true;
+            neotree = true;
+            harpoon = true;
+            gitsigns = true;
+            which_key = true;
+            illuminate = { enabled = true; };
+            treesitter = true;
+            treesitter_context = true;
+            telescope.enabled = true;
+            indent_blankline.enabled = true;
+            mini.enabled = true;
+            native_lsp = {
+              enabled = true;
+              inlay_hints = { background = true; };
+              underlines = {
+                errors = [ "underline" ];
+                hints = [ "underline" ];
+                information = [ "underline" ];
+                warnings = [ "underline" ];
+              };
+            };
+          };
+        };
       };
     };
   };
--- home/programs/nvim/plugins/ui.nix
+++ home/programs/nvim/plugins/ui.nix
@@ -9,14 +9,54 @@
       Headline1.bold = true;
     };
     plugins = {
-      lualine.enable = true;
+      lualine = {
+        enable = true;
+        settings = {
+          options = {
+            sections = {
+              lualine_a = [ "mode" ];
+              lualine_b = [ ];
+              lualine_c = [ ];
+              lualine_y = [{
+                __unkeyed-1 = "aerial";
+                colored = true;
+                cond = {
+                  __raw = ''
+                    function()
+                      local buf_size_limit = 1024 * 1024
+                      if vim.api.nvim_buf_get_offset(0, vim.api.nvim_buf_line_count(0)) > buf_size_limit then
+                        return false
+                      end
+
+                      return true
+                    end
+                  '';
+                };
+                dense = false;
+                dense_sep = ".";
+                depth = { __raw = "nil"; };
+                sep = " ) ";
+              }];
+              lualine_z = [{ __unkeyed-1 = "location"; }];
+            };
+            tabline = {
+              lualine_a = [{
+                __unkeyed-1 = "buffers";
+                symbols = { alternate_file = ""; };
+              }];
+              lualine_z = [ "tabs" ];
+            };
+          };
+        };
+      };
+      dap.enable = true;
       web-devicons.enable = true;
       noice.enable = true;
       gitsigns = {
         enable = true;
         settings.current_line_blame = false;
       };
-      bufferline.enable = true;
+      # bufferline.enable = true;
       trouble.enable = true;
       indent-blankline.enable = true;
       which-key.enable = true;
