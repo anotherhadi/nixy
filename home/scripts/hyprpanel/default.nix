b738189d0c974282790211b3c7639d7b4ddc2a11
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-10-07 20:41:30 +0200

    Update
    Former-commit-id: 52e267a43691dd4b84044f8ddc57a62e4126d3b3

--- home/scripts/hyprpanel/default.nix
+++ home/scripts/hyprpanel/default.nix
@@ -14,12 +14,33 @@ let
     hyprpanel -t bar-3
   '';
 
+  hyprpanel-hide = pkgs.writeShellScriptBin "hyprpanel-hide" ''
+    status=$(hyprpanel -r "isWindowVisible('bar-0')")
+    if [[ $status == "true" ]]; then
+      hyprpanel -t bar-0
+    fi
+    status=$(hyprpanel -r "isWindowVisible('bar-1')")
+    if [[ $status == "true" ]]; then
+      hyprpanel -t bar-1
+    fi
+  '';
+
+  hyprpanel-show = pkgs.writeShellScriptBin "hyprpanel-show" ''
+    status=$(hyprpanel -r "isWindowVisible('bar-0')")
+    if [[ $status == "false" ]]; then
+      hyprpanel -t bar-0
+    fi
+    status=$(hyprpanel -r "isWindowVisible('bar-1')")
+    if [[ $status == "false" ]]; then
+      hyprpanel -t bar-1
+    fi
+  '';
+
   hyprpanel-reload = pkgs.writeShellScriptBin "hyprpanel-reload" ''
     [ $(pgrep "ags") ] && pkill ags
     hyprctl dispatch exec hyprpanel
   '';
-
-  hyprpanel-kill = pkgs.writeShellScriptBin "hyprpanel-kill" ''
-    [ $(pgrep "ags") ] && pkill ags
-  '';
-in { home.packages = [ hyprpanel-toggle hyprpanel-reload hyprpanel-kill ]; }
+in {
+  home.packages =
+    [ hyprpanel-toggle hyprpanel-reload hyprpanel-hide hyprpanel-show ];
+}
