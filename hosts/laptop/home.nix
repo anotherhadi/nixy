7faec62f69a3fdf0d92510a8ce4f1afae6284d4a
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-10-14 21:10:16 +0200

    Init KDE Connect
    Former-commit-id: 52d7a9f2f0501761c7c4c8d3a4344c0f423967f0

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -33,6 +33,7 @@
     ../../home/system/mime
     ../../home/system/udiskie
     ../../home/system/clipman
+    ../../home/system/kdeconnect
 
     ./secrets # CHANGEME: You should probably remove this line, this is where I store my secrets
   ];
