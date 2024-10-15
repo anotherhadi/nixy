4e2c6cbce572575267d18db2672a1af24de11c5c
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-10-15 14:14:12 +0200

    hyprshot -> grimblast
    Former-commit-id: 3910bef1bff11cb16c6f1c622f188d64a1d7dd52

--- home/scripts/screenshot/default.nix
+++ home/scripts/screenshot/default.nix
@@ -1,6 +1,6 @@
 # - ## Screenshot
 #-
-#- This module provides a script to take screenshots using `hyprshot` and `swappy`.
+#- This module provides a script to take screenshots using `grimblast` and `swappy`.
 #-
 #- - `screenshot [region|window|monitor] [swappy]` - Take a screenshot of the region, window, or monitor. Optionally, use `swappy` to copy the screenshot to the clipboard.
 { pkgs, ... }:
@@ -9,28 +9,26 @@ let
     if [[ $2 == "swappy" ]];then
       folder="/tmp"
     else
-      folder="$HOME/Pictures/screenshots"
+      folder="$HOME/Pictures"
     fi
     filename="$(date +%Y-%m-%d_%H:%M:%S).png"
-    mode="region" # Default to region
 
     if [[ $1 == "window" ]];then
-      mode="window"
+      mode="active"
+    elif [[ $1 == "region" ]];then
+      mode="area"
     elif [[ $1 == "monitor" ]];then
       mode="output"
     fi
 
-    ${pkgs.hyprshot}/bin/hyprshot -m $mode -o $folder -f $filename -s || exit 1
+    ${pkgs.grimblast}/bin/grimblast --notify --freeze save $mode "$folder/$filename" || exit 1
 
     if [[ $2 == "swappy" ]];then
-      ${pkgs.swappy}/bin/swappy -f "$folder/$filename" -o "$HOME/Pictures/screenshots/$filename"
+      ${pkgs.swappy}/bin/swappy -f "$folder/$filename" -o "$HOME/Pictures/$filename"
       exit 0
     fi
-
-    message="  Screenshot saved"
-
-    ${pkgs.libnotify}/bin/notify-send "$message" \
-                --icon="$folder/$filename" \
-                --replace-id="$(cat "/tmp/nixy-notification" 2>/dev/null || echo 0)" --print-id > "/tmp/nixy-notification"
   '';
-in { home.packages = [ pkgs.hyprshot screenshot ]; }
+in {
+  home.packages =
+    [ pkgs.hyprshot screenshot pkgs.slurp pkgs.grim pkgs.grimblast ];
+}
