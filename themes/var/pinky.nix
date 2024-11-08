d2aa568fffe17dbf3fb2886d94b0a2a706f44c4b
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-11-08 14:18:55 +0100

    new theme

---
+++ themes/var/pinky.nix
@@ -0,0 +1,18 @@
+{
+  rounding = 25;
+  gaps-in = 12;
+  gaps-out = 12 * 2;
+  active-opacity = 1;
+  inactive-opacity = 0.87;
+  blur = true;
+  border-size = 3;
+  animation-speed = "slow"; # "fast" | "medium" | "slow"
+  fetch = "nerdfetch"; # "nerdfetch" | "neofetch" | "pfetch" | "none"
+
+  bar = {
+    position = "top"; # "top" | "bottom"
+    transparent = true;
+    transparentButtons = false;
+    floating = true;
+  };
+}
