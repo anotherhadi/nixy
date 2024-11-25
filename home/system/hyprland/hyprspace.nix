de8de47de44a6bb11c7721a27690cdd8fbf116e4
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-11-25 09:59:45 +0100

    move hyprspace to file

---
+++ home/system/hyprland/hyprspace.nix
@@ -0,0 +1,7 @@
+{ inputs, pkgs, ... }: {
+
+  wayland.windowManager.hyprland = {
+    plugins = [ inputs.hyprspace.packages.${pkgs.system}.Hyprspace ];
+    settings = { plugin = { overview = { autoDrag = false; }; }; };
+  };
+}
