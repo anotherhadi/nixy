916a9ed923e3759012206317a38efaa957b5737d
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-12-03 19:36:15 +0100

    Switch from Firefox to Zen, ...
    Former-commit-id: 7add142ab10ae16729d15c83c042cf9b96f62223

--- home/system/mime/default.nix
+++ home/system/mime/default.nix
@@ -23,10 +23,10 @@
       "image/jpeg" = "imv.desktop";
       "image/jpg" = "imv.desktop";
       "image/webp" = "imv.desktop";
-      "image/gif" = "firefox.desktop";
-      "x-scheme-handler/http" = "firefox.desktop";
-      "x-scheme-handler/https" = "firefox.desktop";
-      "text/html" = "firefox.desktop";
+      "image/gif" = "zen.desktop";
+      "x-scheme-handler/http" = "zen.desktop";
+      "x-scheme-handler/https" = "zen.desktop";
+      "text/html" = "zen.desktop";
       "application/pdf" = "zathura.desktop";
     };
   };
