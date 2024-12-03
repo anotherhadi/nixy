38d7a0a32694c9c0a0ef2ab8c37de72fdc92615f
Hadi <112569860+anotherhadi@users.noreply.github.com>
2024-12-03 20:08:04 +0100

    update
    Former-commit-id: b8920ade11356e0fc9938895dadee277c2ee13b1

--- flake.nix
+++ flake.nix
@@ -24,15 +24,14 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprspace = {
-      url = "github:KZDKM/Hyprspace";
-      inputs.hyprland.follows = "hyprland";
-    };
+    hyprspace = { url = "github:KZDKM/Hyprspace"; };
     nixy-wallpapers = {
       url = "github:anotherhadi/nixy-wallpapers";
       flake = false;
     };
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    # FIXME: This is a temporary fix until the flake is fixed
+    hyprland.url =
+      "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=refs/heads/main&rev=6a8c756ba1f2b3ccb0cfa8192ccdb2f6490b0512";
     hyprpolkitagent.url = "github:hyprwm/hyprpolkitagent";
     hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
     stylix.url = "github:danth/stylix";
--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -15,7 +15,8 @@
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-emoji
-      (nerdfonts.override { fonts = [ "FiraCode" "Meslo" ]; })
+      nerd-fonts.fira-code
+      nerd-fonts.meslo-lg
       openmoji-color
       twemoji-color-font
       inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd
