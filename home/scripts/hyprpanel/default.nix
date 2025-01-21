1a69437e3f0da3b7ec3f63e60ee5556983206c39
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-01-21 20:54:47 +0100

    fix hyprpanel toggleWindow call
    Former-commit-id: 943ea80ac23c86772f857a1e8a851b9dcab6630c

--- home/scripts/hyprpanel/default.nix
+++ home/scripts/hyprpanel/default.nix
@@ -9,31 +9,31 @@
 { pkgs, ... }:
 let
   hyprpanel-toggle = pkgs.writeShellScriptBin "hyprpanel-toggle" ''
-    hyprpanel -t bar-0
-    hyprpanel -t bar-1
-    hyprpanel -t bar-2
-    hyprpanel -t bar-3
+    hyprpanel toggleWindow bar-0
+    hyprpanel toggleWindow bar-1
+    hyprpanel toggleWindow bar-2
+    hyprpanel toggleWindow bar-3
   '';
 
   hyprpanel-hide = pkgs.writeShellScriptBin "hyprpanel-hide" ''
     status=$(hyprpanel isWindowVisible bar-0)
     if [[ $status == "true" ]]; then
-      hyprpanel -t bar-0
+      hyprpanel toggleWindow bar-0
     fi
     status=$(hyprpanel isWindowVisible bar-1)
     if [[ $status == "true" ]]; then
-      hyprpanel -t bar-1
+      hyprpanel toggleWindow bar-1
     fi
   '';
 
   hyprpanel-show = pkgs.writeShellScriptBin "hyprpanel-show" ''
     status=$(hyprpanel isWindowVisible bar-0)
     if [[ $status == "false" ]]; then
-      hyprpanel -t bar-0
+      hyprpanel toggleWindow bar-0
     fi
     status=$(hyprpanel isWindowVisible bar-1)
     if [[ $status == "false" ]]; then
-      hyprpanel -t bar-1
+      hyprpanel toggleWindow bar-1
     fi
   '';
 
