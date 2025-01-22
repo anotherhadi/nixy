44a8ac06ec6a6f490a6a4e8cc747fed5329644c6
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-01-22 19:41:09 +0100

    edit theme

--- themes/var/2025.nix
+++ themes/var/2025.nix
@@ -5,7 +5,7 @@
   active-opacity = 1;
   inactive-opacity = 0.93;
   blur = true;
-  border-size = 5;
+  border-size = 3;
   animation-speed = "fast"; # "fast" | "medium" | "slow"
   fetch = "none"; # "nerdfetch" | "neofetch" | "pfetch" | "none"
 
