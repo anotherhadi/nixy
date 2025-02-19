b90bbf0e78562b9e5fb5669bcea44b2236c765be
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-02-19 10:43:08 +0100

    batsignal now locks on very low battery
    Former-commit-id: 3b200caed9205806f27f49d7f93d3490487e612a

--- home/system/batsignal/default.nix
+++ home/system/batsignal/default.nix
@@ -1,7 +1,8 @@
 # Batsignal is a simple utility to send battery notifications.
-{
+{ pkgs, ... }: {
   services.batsignal = {
     enable = true;
-    extraArgs = [ "-c 10" "-w 30" "-f disabled" ];
+    extraArgs =
+      [ "-c 10" "-w 30" "-f disabled" "-D ${pkgs.hyprlock}/bin/hyprlock" ];
   };
 }
