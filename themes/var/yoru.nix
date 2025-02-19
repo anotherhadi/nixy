16c41a45ac26c7a32b4d702fc1cb71f5eb4a317e
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-02-19 14:55:08 +0100

    increase opacity for inactive windows
    Former-commit-id: bac6ec2f82a4570068252e030c0e0cd2e7c12e7e

--- themes/var/yoru.nix
+++ themes/var/yoru.nix
@@ -3,7 +3,7 @@
   gaps-in = 9;
   gaps-out = 9 * 2;
   active-opacity = 1;
-  inactive-opacity = 0.93;
+  inactive-opacity = 0.98;
   blur = true;
   border-size = 3;
   animation-speed = "fast"; # "fast" | "medium" | "slow"
