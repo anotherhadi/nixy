8656e31386a53408982a63669abbd6dfdb4b781a
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-02-23 11:48:03 +0100

    edit xdg portals
    Former-commit-id: 14d8e501004a17b8008043e65911a6da2bd7c9d8

--- nixos/xdg-portal.nix
+++ nixos/xdg-portal.nix
@@ -4,7 +4,11 @@
     config.common.default = "*";
     wlr.enable = true;
     xdgOpenUsePortal = true;
-    extraPortals =
-      [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.libsForQt5.xdg-desktop-portal-kde
+    ];
+    config.hyprland = { default = [ "hyprland" "gtk" ]; };
   };
 }
