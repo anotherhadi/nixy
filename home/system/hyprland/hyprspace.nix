71ec7d1390c35f52430a81e914899739f5411e90
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-04 21:42:49 +0100

    remove the hyprspace shortcut
    Former-commit-id: 093a6397b9a4a0965f75790b42e8090656e5a124

--- home/system/hyprland/hyprspace.nix
+++ home/system/hyprland/hyprspace.nix
@@ -1,11 +1,6 @@
 { inputs, pkgs, ... }: {
   wayland.windowManager.hyprland = {
     plugins = [ inputs.hyprspace.packages.${pkgs.system}.Hyprspace ];
-    settings = {
-      plugin = { overview = { autoDrag = false; }; };
-      bind = [
-        "$mod,TAB, overview:toggle" # Overview
-      ];
-    };
+    settings = { plugin = { overview = { autoDrag = false; }; }; };
   };
 }
