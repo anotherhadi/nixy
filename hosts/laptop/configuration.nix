a83a9a2c424c326fdaef7e6a66f04a0dc8b7228c
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-18 21:52:32 +0100

    go back to systemd-boot
    Former-commit-id: 5a8bdffc2a5de802e2a7889e6b2f1f9f61a8d642

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -7,8 +7,8 @@
     ../../nixos/fonts.nix
     ../../nixos/home-manager.nix
     ../../nixos/nix.nix
-    # ../../nixos/systemd-boot.nix
-    ../../nixos/grub.nix
+    ../../nixos/systemd-boot.nix
+    # ../../nixos/grub.nix
     ../../nixos/sddm.nix
     ../../nixos/users.nix
     ../../nixos/utils.nix
