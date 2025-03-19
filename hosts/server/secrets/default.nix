1cdeb2dfb6e6109e7a24c841ebcd3056097a00dd
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-19 10:11:56 +0100

    testing
    Former-commit-id: 8242da9bcbebec22b6701d577685ff0acc724745

--- hosts/server/secrets/default.nix
+++ hosts/server/secrets/default.nix
@@ -1,15 +1,12 @@
 { inputs, ... }: {
-  imports = [ inputs.sops-nix.nixosModules.sops ];
-
   sops = {
     age.keyFile = "/home/hadi/.config/sops/age/keys.txt";
     defaultSopsFile = ./secrets.yaml;
     secrets = {
       "example" = {
         owner = "hadi";
-        path = "/home/hadi/test";
+        path = "/etc/test";
       };
     };
   };
-
 }
