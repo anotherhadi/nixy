78245dc92b48cc8c2f4bbd4ed49da561152a242f
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-19 18:25:38 +0100

    woops

--- hosts/server/secrets/default.nix
+++ hosts/server/secrets/default.nix
@@ -15,7 +15,7 @@
       };
       cloudflare-dns-token = { path = "/etc/cloudflare/dnskey.txt"; };
       nextcloud-pwd = { path = "/etc/nextcloud/pwd.txt"; };
-      adguard-pwd = { owner = "glance"; };
+      adguard-pwd = { };
     };
   };
 }
