1a1b5e9d079b70f73775927cf2c1c56fe74fbe07
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-22 17:46:47 +0100

    update mimeapps
    Former-commit-id: 8c43e650004bace0bf14637c77f7343e47797b93

--- home/system/mime/default.nix
+++ home/system/mime/default.nix
@@ -1,40 +1,49 @@
 # Mime allows us to configure the default applications for each file type
 {
-  xdg.mimeApps = {
+  xdg = {
     enable = true;
-    defaultApplications = {
-      "text/markdown" = "nvim.desktop";
-      "text/plain" = "nvim.desktop";
-      "text/x-shellscript" = "nvim.desktop";
-      "text/x-python" = "nvim.desktop";
-      "text/x-go" = "nvim.desktop";
-      "text/css" = "nvim.desktop";
-      "text/javascript" = "nvim.desktop";
-      "text/x-c" = "nvim.desktop";
-      "text/x-c++" = "nvim.desktop";
-      "text/x-java" = "nvim.desktop";
-      "text/x-rust" = "nvim.desktop";
-      "text/x-yaml" = "nvim.desktop";
-      "text/x-toml" = "nvim.desktop";
-      "text/x-dockerfile" = "nvim.desktop";
-      "text/x-xml" = "nvim.desktop";
-      "text/x-php" = "nvim.desktop";
-      "image/jpeg" = "imv.desktop";
-      "image/jpg" = "imv.desktop";
-      "image/webp" = "imv.desktop";
-      "image/gif" = "zen.desktop";
-      "x-scheme-handler/http" = "zen.desktop";
-      "x-scheme-handler/https" = "zen.desktop";
-      "text/html" = "zen.desktop";
-      "application/pdf" = "zathura.desktop";
-      "image/png" = "imv-dir.desktop";
-      "x-scheme-handler/chrome" = "zen.desktop";
-      "application/x-extension-htm" = "zen.desktop";
-      "application/x-extension-html" = "zen.desktop";
-      "application/x-extension-shtml" = "zen.desktop";
-      "application/xhtml+xml" = "zen.desktop";
-      "application/x-extension-xhtml" = "zen.desktop";
-      "application/x-extension-xht" = "zen.desktop";
+    mime.enable = true;
+    userDirs.enable = true;
+    portal.xdgOpenUsePortal = true;
+    configFile."mimeapps.list".force =
+      true; # don't error when mimeapps.list is replaced, it gets replaced often
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/markdown" = "nvim.desktop";
+        "text/plain" = "nvim.desktop";
+        "text/x-shellscript" = "nvim.desktop";
+        "text/x-python" = "nvim.desktop";
+        "text/x-go" = "nvim.desktop";
+        "text/css" = "nvim.desktop";
+        "text/javascript" = "nvim.desktop";
+        "text/x-c" = "nvim.desktop";
+        "text/x-c++" = "nvim.desktop";
+        "text/x-java" = "nvim.desktop";
+        "text/x-rust" = "nvim.desktop";
+        "text/x-yaml" = "nvim.desktop";
+        "text/x-toml" = "nvim.desktop";
+        "text/x-dockerfile" = "nvim.desktop";
+        "text/x-xml" = "nvim.desktop";
+        "text/x-php" = "nvim.desktop";
+        "image/jpeg" = "imv.desktop";
+        "image/jpg" = "imv.desktop";
+        "image/webp" = "imv.desktop";
+        "image/gif" = "zen.desktop";
+        "x-scheme-handler/http" = "zen.desktop";
+        "x-scheme-handler/https" = "zen.desktop";
+        "text/html" = "zen.desktop";
+        "inode/directory" = "thunar.desktop";
+        "application/pdf" = "zathura.desktop";
+        "image/png" = "imv-dir.desktop";
+        "x-scheme-handler/chrome" = "zen.desktop";
+        "application/x-extension-htm" = "zen.desktop";
+        "application/x-extension-html" = "zen.desktop";
+        "application/x-extension-shtml" = "zen.desktop";
+        "application/xhtml+xml" = "zen.desktop";
+        "application/x-extension-xhtml" = "zen.desktop";
+        "application/x-extension-xht" = "zen.desktop";
+      };
     };
   };
 }
