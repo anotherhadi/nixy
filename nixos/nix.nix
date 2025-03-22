74072c04fc10fd25ea8995c7f760d4adbf9d8c27
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-22 13:20:42 +0100

    add trusted nix caches
    Former-commit-id: d438abe210a9b22e318038add46566d80a3fe032

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -18,10 +18,14 @@ in {
         "https://cache.nixos.org/"
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
+        "https://cache.garnix.io"
+        "https://numtide.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
     };
     gc = {
