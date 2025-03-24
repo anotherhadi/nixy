829debe3315c20aa873bebf62a3cb245daba2c20
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-24 13:18:55 +0100

    run tailscale without password
    Former-commit-id: d495ce1d5766e840dbf3ef1895e09f4c727b02eb

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -1,1 +1,16 @@
-{ services.tailscale = { enable = true; }; }
+{ config, ... }: {
+  security.sudo.extraRules = [{
+    users = [ config.var.username ];
+    commands = [
+      {
+        command = "/etc/profiles/per-user/${config.var.username}/bin/tailscale";
+        options = [ "NOPASSWD" ];
+      }
+      {
+        command = "/run/current-system/sw/bin/tailscale";
+        options = [ "NOPASSWD" ];
+      }
+    ];
+  }];
+  services.tailscale = { enable = true; };
+}
