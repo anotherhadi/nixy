fffd17a39c96a3c0f9d0a9a4d5e1db596e7f760e
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-24 13:15:10 +0100

    mealie init
    Former-commit-id: bb02792bf4d3738846b62b54d88bb63b4bded5ec

---
+++ server-modules/mealie.nix
@@ -0,0 +1,18 @@
+{ config, ... }:
+let domain = "mealie.hadi.diy";
+in {
+  services = {
+    mealie = {
+      enable = true;
+      port = 8092;
+    };
+
+    nginx.virtualHosts."${domain}" = {
+      useACMEHost = "hadi.diy";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.mealie.port}";
+      };
+    };
+  };
+}
