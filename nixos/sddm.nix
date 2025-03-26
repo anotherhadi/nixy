623b64d230d7ab6443227a585dbf9522ba7cadd7
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-26 17:00:25 +0100

    Change theme
    Former-commit-id: 3f1421b4917cf0dca7bc45c9e2da8749e0b26d55

--- nixos/sddm.nix
+++ nixos/sddm.nix
@@ -4,9 +4,30 @@ let
   foreground = config.theme.textColorOnWallpaper;
   sddm-astronaut = pkgs.sddm-astronaut.override {
     embeddedTheme = "pixel_sakura";
+    # TODO: Cleaner version for static vs .gif, just a string.replace
     themeConfig = if lib.hasSuffix "sakura_static.png" config.stylix.image then
       { }
-    else {
+    else if lib.hasSuffix "studio.png" config.stylix.image then {
+      Background = pkgs.fetchurl {
+        url =
+          "https://raw.githubusercontent.com/anotherhadi/nixy-wallpapers/refs/heads/main/wallpapers/studio.gif";
+        sha256 = "sha256-qySDskjmFYt+ncslpbz0BfXiWm4hmFf5GPWF2NlTVB8=";
+      };
+      HeaderTextColor = "#${foreground}";
+      DateTextColor = "#${foreground}";
+      TimeTextColor = "#${foreground}";
+      LoginFieldTextColor = "#${foreground}";
+      PasswordFieldTextColor = "#${foreground}";
+      UserIconColor = "#${foreground}";
+      PasswordIconColor = "#${foreground}";
+      WarningColor = "#${foreground}";
+      LoginButtonBackgroundColor = "#${foreground}";
+      SystemButtonsIconsColor = "#${foreground}";
+      SessionButtonTextColor = "#${foreground}";
+      VirtualKeyboardButtonTextColor = "#${foreground}";
+      DropdownBackgroundColor = "#${foreground}";
+      HighlightBackgroundColor = "#${foreground}";
+    } else {
       Background = "${toString config.stylix.image}";
       HeaderTextColor = "#${foreground}";
       DateTextColor = "#${foreground}";
