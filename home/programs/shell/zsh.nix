2f5e5683e9d3fb50c435ec0bb01912e4406e9308
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-27 15:39:06 +0100

    zsh config update
    Former-commit-id: ffa3817383ec8149118956394b2313b6517776b5

--- home/programs/shell/zsh.nix
+++ home/programs/shell/zsh.nix
@@ -11,7 +11,11 @@ in {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+    syntaxHighlighting = {
+      enable = true;
+      highlighters =
+        [ "main" "brackets" "pattern" "cursor" "regexp" "root" "line" ];
+    };
     historySubstringSearch.enable = true;
 
     initExtraFirst = ''
@@ -76,6 +80,8 @@ in {
       open = "${pkgs.xdg-utils}/bin/xdg-open";
       icat = "${pkgs.kitty}/bin/kitty +kitten icat";
       ssh = "kitty +kitten ssh";
+      cat =
+        "bat --theme=base16 --color=always --paging=never --tabs=2 --wrap=never --plain";
 
       wireguard-import = "nmcli connection import type wireguard file";
 
@@ -101,5 +107,78 @@ in {
       gaa = "git add .";
       gcm = "git commit -m";
     };
+
+    initExtra = ''
+      # search history based on what's typed in the prompt
+      autoload -U history-search-end
+      zle -N history-beginning-search-backward-end history-search-end
+      zle -N history-beginning-search-forward-end history-search-end
+      bindkey "^[OA" history-beginning-search-backward-end
+      bindkey "^[OB" history-beginning-search-forward-end
+
+      # General completion behavior
+      zstyle ':completion:*' completer _extensions _complete _approximate
+
+      # Use cache
+      zstyle ':completion:*' use-cache on
+      zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+
+      # Complete the alias
+      zstyle ':completion:*' complete true
+
+      # Autocomplete options
+      zstyle ':completion:*' complete-options true
+
+      # Completion matching control
+      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+      zstyle ':completion:*' keep-prefix true
+
+      # Group matches and describe
+      zstyle ':completion:*' menu select
+      zstyle ':completion:*' list-grouped false
+      zstyle ':completion:*' list-separator '''
+      zstyle ':completion:*' group-name '''
+      zstyle ':completion:*' verbose yes
+      zstyle ':completion:*:matches' group 'yes'
+      zstyle ':completion:*:warnings' format '%F{red}%B-- No match for: %d --%b%f'
+      zstyle ':completion:*:messages' format '%d'
+      zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
+      zstyle ':completion:*:descriptions' format '[%d]'
+
+      # Colors
+      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+
+      # case insensitive tab completion
+      zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+      zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+      zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
+      zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
+      zstyle ':completion:*' special-dirs true
+      zstyle ':completion:*' squeeze-slashes true
+
+      # Sort
+      zstyle ':completion:*' sort false
+      zstyle ":completion:*:git-checkout:*" sort false
+      zstyle ':completion:*' file-sort modification
+      zstyle ':completion:*:eza' sort false
+      zstyle ':completion:complete:*:options' sort false
+      zstyle ':completion:files' sort false
+
+      ${lib.optionalString config.services.gpg-agent.enable ''
+        gnupg_path=$(ls $XDG_RUNTIME_DIR/gnupg)
+        export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gnupg/$gnupg_path/S.gpg-agent.ssh"
+      ''}
+
+      # Allow foot to pipe command output
+      function precmd {
+        if ! builtin zle; then
+            print -n "\e]133;D\e\\"
+        fi
+      }
+      function preexec {
+        print -n "\e]133;C\e\\"
+      }
+
+    '';
   };
 }
