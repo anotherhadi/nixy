2d76df1b851e66e6494287ca045c8d7d1eac2676
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-27 15:39:26 +0100

    remove batsignal

--- home/system/batsignal/default.nix
+++
@@ -1,8 +0,0 @@
-# Batsignal is a simple utility to send battery notifications.
-{ pkgs, ... }: {
-  services.batsignal = {
-    enable = true;
-    extraArgs =
-      [ "-c 10" "-w 30" "-f disabled" "-D ${pkgs.hyprlock}/bin/hyprlock" ];
-  };
-}
