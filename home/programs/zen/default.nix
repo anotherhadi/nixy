f894f2732644c8d4e16daae1a63b3847d5219610
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-27 15:39:14 +0100

    zen config update
    Former-commit-id: d6d7bb9b9bf19fbee80f5146f8239c6d9a839e64

--- home/programs/zen/default.nix
+++ home/programs/zen/default.nix
@@ -1,5 +1,14 @@
 # Zen is a minimalistic web browser.
-{ pkgs, inputs, ... }: {
-  home.packages = with pkgs;
-    [ inputs.zen-browser.packages."${system}".default ];
-}
+{ pkgs, inputs, ... }:
+let
+  # Create a wrapper script for zen-browser with Wayland enabled
+  zenWithWayland = pkgs.symlinkJoin {
+    name = "zen-browser-wayland";
+    paths = [ inputs.zen-browser.packages."${pkgs.system}".default ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/zen \
+        --set MOZ_ENABLE_WAYLAND 1
+    '';
+  };
+in { home.packages = [ zenWithWayland ]; }
