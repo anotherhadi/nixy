0398d077e5d7066d7185af5e4488f51639d0f1e2
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-04-05 14:04:06 +0200

    fix search-nix-api

--- server-modules/search-nixos-api.nix
+++ server-modules/search-nixos-api.nix
@@ -2,17 +2,14 @@
 let domain = "search-nixos-api.hadi.diy";
 in {
   services = {
-    nixos-search-api = {
-      enable = true;
-      port = 8089;
-    };
+    search-nixos-api = { enable = true; };
 
     nginx.virtualHosts."${domain}" = {
       useACMEHost = "hadi.diy";
       forceSSL = true;
       locations."/" = {
         proxyPass =
-          "http://127.0.0.1:${toString config.services.nixos-search-api.port}";
+          "http://127.0.0.1:${toString config.services.search-nixos-api.port}/";
       };
     };
   };
