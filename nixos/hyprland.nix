5c7d3431bd1a2402fabe7466ec0d787fb57b7645
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-04-18 14:58:38 +0200

    fix package panel
    Former-commit-id: 6ce19fb66330698651c7a65d6365c876653e41c6

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,6 +1,9 @@
-{
+{ inputs, pkgs, ... }: {
   programs.hyprland = {
     enable = true;
     withUWSM = true;
+    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 }
