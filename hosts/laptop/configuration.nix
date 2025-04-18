31e03a7fb8720c5edfcb8a364901fefc7681af45
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-04-18 10:18:05 +0200

    Init hp-omen-linux-module to controle the keyboard backlight on omen laptop
    Former-commit-id: d69b8c39e37144528411b7c19a6a729a671a8204

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -14,6 +14,7 @@
     ../../nixos/utils.nix
     # ../../nixos/docker.nix
     ../../nixos/tailscale.nix
+    ../../nixos/omen.nix # For my laptop only
 
     # You should let those lines as is
     ./hardware-configuration.nix
