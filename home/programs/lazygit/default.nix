53e8a42484130d4be27188ffcfb64c8c75a4c203
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-05-07 15:39:50 +0200

    update lazygit config
    Former-commit-id: 4bc6576d7c85fbbf5623aaa9783b70448e93d096

--- home/programs/lazygit/default.nix
+++ home/programs/lazygit/default.nix
@@ -7,6 +7,14 @@ in {
   programs.lazygit = {
     enable = true;
     settings = lib.mkForce {
+      disableStartupPopups = true;
+      notARepository = "skip";
+      promptToReturnFromSubprocess = false;
+      update.method = "never";
+      git = {
+        commit.signOff = true;
+        parseEmoji = true;
+      };
       gui = {
         theme = {
           activeBorderColor = [ accent "bold" ];
