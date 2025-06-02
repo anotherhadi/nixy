76a5931da7e21b0cc836520fda13410b4ac6339f
Ilia Bushmakin <im.bushmakin@gmail.com>
2025-06-02 21:35:23 +0400

    feat: change default keyboard layout

--- hosts/aepc/variables.nix
+++ hosts/aepc/variables.nix
@@ -10,7 +10,7 @@
     configDirectory = "/home/" + config.var.username
       + "/.config/nixos"; # The path of the nixos configuration directory
 
-    keyboardLayout = "us";
+    keyboardLayout = "en";
 
     location = "Abu Dhabi";
     timeZone = "Asia/Dubai";
