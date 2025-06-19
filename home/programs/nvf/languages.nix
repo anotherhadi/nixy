f306041af9819615f48318050e58de607f90ba15
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-06-19 15:29:24 +0200

    edit nvf config
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>
    
    Former-commit-id: 20350cacd5f393f214438f5b19753af53b56f305

--- home/programs/nvf/languages.nix
+++ home/programs/nvf/languages.nix
@@ -1,28 +1,84 @@
-{
+{lib, ...}: {
   programs.nvf.settings.vim = {
     diagnostics = {
       enable = true;
-      nvim-lint.enable = true;
-      config.virtual_text = true;
+      config = {
+        signs = {
+          text = {
+            "vim.diagnostic.severity.Error" = " ";
+            "vim.diagnostic.severity.Warn" = " ";
+            "vim.diagnostic.severity.Hint" = " ";
+            "vim.diagnostic.severity.Info" = " ";
+          };
+        };
+        underline = true;
+        update_in_insert = true;
+        virtual_text = {
+          format =
+            lib.generators.mkLuaInline
+            /*
+            lua
+            */
+            ''
+              function(diagnostic)
+                return string.format("%s", diagnostic.message)
+                --return string.format("%s (%s)", diagnostic.message, diagnostic.source)
+              end
+            '';
+        };
+      };
+      nvim-lint = {
+        enable = true;
+        linters = {
+        };
+        linters_by_ft = {
+          c = [
+            "cppcheck"
+          ];
+        };
+      };
     };
     syntaxHighlighting = true;
     treesitter = {
       enable = true;
       autotagHtml = true;
       context.enable = true;
-      highlight = {
-        enable = true;
-        additionalVimRegexHighlighting = true;
-      };
+      highlight.enable = true;
     };
     lsp = {
       enable = true;
       trouble.enable = true;
+      lspSignature.enable = true;
+      lspconfig.enable = true;
       formatOnSave = true;
       inlayHints.enable = true;
-      lightbulb.enable = true;
       null-ls.enable = true;
-      otter-nvim.enable = true;
+      otter-nvim = {
+        enable = true;
+        setupOpts = {
+          buffers.set_filetype = true;
+          lsp = {
+            diagnostic_update_event = [
+              "BufWritePost"
+              "InsertLeave"
+            ];
+          };
+        };
+      };
+      lspkind.enable = true;
+      lspsaga = {
+        enable = true;
+        setupOpts = {
+          ui = {
+            code_action = "🟅";
+          };
+          lightbulb = {
+            sign = false;
+            virtual_text = true;
+          };
+          breadcrumbs.enable = false;
+        };
+      };
     };
     languages = {
       enableDAP = true;
@@ -32,15 +88,30 @@
 
       astro.enable = true;
       go.enable = true;
-      markdown.enable = true;
-      ts.enable = true;
-      ts.extensions.ts-error-translator.enable = true;
+      markdown = {
+        enable = true;
+        extensions = {
+          render-markdown-nvim = {
+            enable = true;
+          };
+        };
+        extraDiagnostics.enable = true;
+      };
+      ts = {
+        enable = true;
+        extensions.ts-error-translator.enable = true;
+      };
       css.enable = true;
       svelte.enable = true;
       html.enable = true;
       bash.enable = true;
       nix.enable = true;
       tailwind.enable = true;
+    };
+    formatter = {
+      conform-nvim = {
+        enable = true;
+      };
     };
   };
 }
