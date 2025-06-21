6649f9375419042ec38b8e9140bfb8e53b6072ac
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-06-21 14:28:12 +0200

    add plymouth
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>
    
    Former-commit-id: 5e67a0bcd2c02d201cf8d074b5fc29aca2e41d0e

--- nixos/systemd-boot.nix
+++ nixos/systemd-boot.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   boot = {
     bootspec.enable = true;
     loader = {
@@ -17,12 +21,22 @@
     kernelParams = [
       "quiet"
       "splash"
-      "vga=current"
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
+      "boot.shell_on_fail"
     ];
     consoleLogLevel = 0;
     initrd.verbose = false;
+
+    plymouth = {
+      enable = true;
+      theme = lib.mkForce "cuts_alt";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["cuts_alt"];
+        })
+      ];
+    };
   };
 }
