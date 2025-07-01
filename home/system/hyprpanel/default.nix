0bfcb20fda29aa2835fd9c61a23df984057c9e53
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-07-01 17:36:22 +0200

    update flake & fix hyprpanel
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>
    
    Former-commit-id: eebaa0ffc17af2e314bba4c7161a9b76ffd03cd8

--- home/system/hyprpanel/default.nix
+++ home/system/hyprpanel/default.nix
@@ -1,7 +1,6 @@
 # Hyprpanel is the bar on top of the screen
-# Display informations like workspaces, battery, wifi, ...
-{ inputs, config, ... }:
-let
+# Display information like workspaces, battery, wifi, ...
+{config, ...}: let
   transparentButtons = config.theme.bar.transparentButtons;
 
   accent = "#${config.lib.stylix.colors.base0D}";
@@ -27,21 +26,22 @@ let
 
   location = config.var.location;
 in {
-
-  imports = [ inputs.hyprpanel.homeManagerModules.hyprpanel ];
-
   programs.hyprpanel = {
     enable = true;
-    hyprland.enable = true;
-    overwrite.enable = true;
-    overlay.enable = true;
 
     settings = {
       layout = {
-        "bar.layouts" = {
+        bar.layouts = {
           "*" = {
-            "left" = [ "dashboard" "workspaces" "windowtitle" ];
-            "middle" = [ "media" "cava" ];
+            "left" = [
+              "dashboard"
+              "workspaces"
+              "windowtitle"
+            ];
+            "middle" = [
+              "media"
+              "cava"
+            ];
             "right" = [
               "systray"
               "volume"
@@ -59,28 +59,48 @@ in {
       theme.font.size = fontSizeForHyprpanel;
 
       theme.bar.outer_spacing =
-        if floating && transparent then "0px" else "8px";
+        if floating && transparent
+        then "0px"
+        else "8px";
       theme.bar.buttons.y_margins =
-        if floating && transparent then "0px" else "8px";
+        if floating && transparent
+        then "0px"
+        else "8px";
       theme.bar.buttons.spacing = "0.3em";
       theme.bar.buttons.radius =
-        (if transparent then toString rounding else toString (rounding - 8))
+        (
+          if transparent
+          then toString rounding
+          else toString (rounding - 8)
+        )
         + "px";
       theme.bar.floating = floating;
       theme.bar.buttons.padding_x = "0.8rem";
       theme.bar.buttons.padding_y = "0.4rem";
 
       theme.bar.margin_top =
-        (if position == "top" then toString (gaps-in * 2) else "0") + "px";
+        (
+          if position == "top"
+          then toString (gaps-in * 2)
+          else "0"
+        )
+        + "px";
       theme.bar.margin_bottom =
-        (if position == "top" then "0" else toString (gaps-in * 2)) + "px";
+        (
+          if position == "top"
+          then "0"
+          else toString (gaps-in * 2)
+        )
+        + "px";
       theme.bar.margin_sides = toString gaps-out + "px";
       theme.bar.border_radius = toString rounding + "px";
       theme.bar.transparent = transparent;
       theme.bar.location = position;
       theme.bar.dropdownGap = "4.5em";
       theme.bar.menus.shadow =
-        if transparent then "0 0 0 0" else "0px 0px 3px 1px #16161e";
+        if transparent
+        then "0 0 0 0"
+        else "0px 0px 3px 1px #16161e";
       theme.bar.buttons.style = "default";
       theme.bar.buttons.monochrome = true;
       theme.bar.menus.monochrome = true;
@@ -146,78 +166,88 @@ in {
       menus.dashboard.shortcuts.right.shortcut1.command = "hyprpicker -a";
       menus.dashboard.shortcuts.right.shortcut1.tooltip = "Color Picker";
       menus.dashboard.shortcuts.right.shortcut3.icon = "󰄀";
-      menus.dashboard.shortcuts.right.shortcut3.command =
-        "screenshot region swappy";
+      menus.dashboard.shortcuts.right.shortcut3.command = "screenshot region swappy";
       menus.dashboard.shortcuts.right.shortcut3.tooltip = "Screenshot";
 
       menus.power.lowBatteryNotification = true;
 
       wallpaper.enable = false;
-    };
 
-    override = {
-      "theme.bar.buttons.workspaces.hover" = accent-alt;
-      "theme.bar.buttons.workspaces.active" = accent;
-      "theme.bar.buttons.workspaces.available" = accent-alt;
-      "theme.bar.buttons.workspaces.occupied" = accent-alt;
+      theme.bar.buttons.workspaces.hover = accent-alt;
+      theme.bar.buttons.workspaces.active = accent;
+      theme.bar.buttons.workspaces.available = accent-alt;
+      theme.bar.buttons.workspaces.occupied = accent-alt;
 
-      "theme.bar.menus.background" = background;
-      "theme.bar.menus.cards" = background-alt;
-      "theme.bar.menus.label" = foreground;
-      "theme.bar.menus.text" = foreground;
-      "theme.bar.menus.border.color" = accent;
-      "theme.bar.menus.popover.text" = foreground;
-      "theme.bar.menus.popover.background" = background-alt;
-      "theme.bar.menus.listitems.active" = accent;
-      "theme.bar.menus.icons.active" = accent;
-      "theme.bar.menus.switch.enabled" = accent;
-      "theme.bar.menus.check_radio_button.active" = accent;
-      "theme.bar.menus.buttons.default" = accent;
-      "theme.bar.menus.buttons.active" = accent;
-      "theme.bar.menus.iconbuttons.active" = accent;
-      "theme.bar.menus.progressbar.foreground" = accent;
-      "theme.bar.menus.slider.primary" = accent;
-      "theme.bar.menus.tooltip.background" = background-alt;
-      "theme.bar.menus.tooltip.text" = foreground;
-      "theme.bar.menus.dropdownmenu.background" = background-alt;
-      "theme.bar.menus.dropdownmenu.text" = foreground;
+      theme.bar.menus.background = background;
+      theme.bar.menus.cards = background-alt;
+      theme.bar.menus.label = foreground;
+      theme.bar.menus.text = foreground;
+      theme.bar.menus.border.color = accent;
+      theme.bar.menus.popover.text = foreground;
+      theme.bar.menus.popover.background = background-alt;
+      theme.bar.menus.listitems.active = accent;
+      theme.bar.menus.icons.active = accent;
+      theme.bar.menus.switch.enabled = accent;
+      theme.bar.menus.check_radio_button.active = accent;
+      theme.bar.menus.buttons.default = accent;
+      theme.bar.menus.buttons.active = accent;
+      theme.bar.menus.iconbuttons.active = accent;
+      theme.bar.menus.progressbar.foreground = accent;
+      theme.bar.menus.slider.primary = accent;
+      theme.bar.menus.tooltip.background = background-alt;
+      theme.bar.menus.tooltip.text = foreground;
+      theme.bar.menus.dropdownmenu.background = background-alt;
+      theme.bar.menus.dropdownmenu.text = foreground;
 
-      "theme.bar.background" = background
-        + (if transparentButtons && transparent then "00" else "");
-      "theme.bar.buttons.text" = if transparent && transparentButtons then
-        foregroundOnWallpaper
-      else
-        foreground;
-      "theme.bar.buttons.background" =
-        (if transparent then background else background-alt)
-        + (if transparentButtons then "00" else "");
-      "theme.bar.buttons.icon" = accent;
+      theme.bar.background =
+        background
+        + (
+          if transparentButtons && transparent
+          then "00"
+          else ""
+        );
+      theme.bar.buttons.text =
+        if transparent && transparentButtons
+        then foregroundOnWallpaper
+        else foreground;
+      theme.bar.buttons.background =
+        (
+          if transparent
+          then background
+          else background-alt
+        )
+        + (
+          if transparentButtons
+          then "00"
+          else ""
+        );
+      theme.bar.buttons.icon = accent;
 
-      "theme.bar.buttons.notifications.background" = background-alt;
-      "theme.bar.buttons.hover" = background;
-      "theme.bar.buttons.notifications.hover" = background;
-      "theme.bar.buttons.notifications.total" = accent;
-      "theme.bar.buttons.notifications.icon" = accent;
+      theme.bar.buttons.notifications.background = background-alt;
+      theme.bar.buttons.hover = background;
+      theme.bar.buttons.notifications.hover = background;
+      theme.bar.buttons.notifications.total = accent;
+      theme.bar.buttons.notifications.icon = accent;
 
-      "theme.osd.bar_color" = accent;
-      "theme.osd.bar_overflow_color" = accent-alt;
-      "theme.osd.icon" = background;
-      "theme.osd.icon_container" = accent;
-      "theme.osd.label" = accent;
-      "theme.osd.bar_container" = background-alt;
+      theme.osd.bar_color = accent;
+      theme.osd.bar_overflow_color = accent-alt;
+      theme.osd.icon = background;
+      theme.osd.icon_container = accent;
+      theme.osd.label = accent;
+      theme.osd.bar_container = background-alt;
 
-      "theme.bar.menus.menu.media.background.color" = background-alt;
-      "theme.bar.menus.menu.media.card.color" = background-alt;
+      theme.bar.menus.menu.media.background.color = background-alt;
+      theme.bar.menus.menu.media.card.color = background-alt;
 
-      "theme.notification.background" = background-alt;
-      "theme.notification.actions.background" = accent;
-      "theme.notification.actions.text" = foreground;
-      "theme.notification.label" = accent;
-      "theme.notification.border" = background-alt;
-      "theme.notification.text" = foreground;
-      "theme.notification.labelicon" = accent;
-      "theme.notification.close_button.background" = background-alt;
-      "theme.notification.close_button.label" = "#f38ba8";
+      theme.notification.background = background-alt;
+      theme.notification.actions.background = accent;
+      theme.notification.actions.text = foreground;
+      theme.notification.label = accent;
+      theme.notification.border = background-alt;
+      theme.notification.text = foreground;
+      theme.notification.labelicon = accent;
+      theme.notification.close_button.background = background-alt;
+      theme.notification.close_button.label = "#f38ba8";
     };
   };
 }
