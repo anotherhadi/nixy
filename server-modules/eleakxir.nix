d7e822335173a8e266fd27077736946b62521248
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-09-24 19:12:35 +0200

    change eleakxir configuration
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- server-modules/eleakxir.nix
+++ server-modules/eleakxir.nix
@@ -1,12 +1,26 @@
-{config, ...}: {
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}: {
+  users.groups.eleakxir = {};
+  users.users.hadi.extraGroups = ["eleakxir"];
+
   services.eleakxir = {
     enable = true;
     port = 9198;
-    folders = ["/var/lib/eleakxir/leaks/"];
+    user = "eleakxir";
+    group = "eleakxir";
+    folders = ["/var/lib/eleakxir/leaks/" "/mnt/data/clean-leak/"];
     debug = true;
   };
 
-  nginx.virtualHosts."eleakxir.hadi.diy" = {
+  environment.systemPackages = [
+    inputs.eleakxir.packages.${pkgs.system}.leak-utils
+  ];
+
+  services.nginx.virtualHosts."eleakxir-back.hadi.diy" = {
     useACMEHost = "hadi.diy";
     forceSSL = true;
     locations."/" = {
