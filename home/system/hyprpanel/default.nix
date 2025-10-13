8910fca468822fb5c0a38f8798eacee7eef6a6c3
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-10-13 12:05:57 +0200

    update
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- home/system/hyprpanel/default.nix
+++ home/system/hyprpanel/default.nix
@@ -27,7 +27,6 @@
   location = config.var.location;
 
   homeDir = "/home/${config.var.username}";
-
 in {
   wayland.windowManager.hyprland.settings.exec-once = ["hyprpanel"];
 
@@ -35,11 +34,10 @@ in {
     enable = true;
 
     settings = {
-
       bar.layouts = {
         "*" = {
-          left = [ "dashboard" "workspaces" "windowtitle" ];
-          middle = [ "media" "cava" ];
+          left = ["dashboard" "workspaces" "windowtitle"];
+          middle = ["media" "cava"];
           right = [
             "systray"
             "volume"
@@ -189,81 +187,81 @@ in {
 
       wallpaper.enable = false;
 
-      theme.bar.buttons.workspaces.hover = accent-alt;
-      theme.bar.buttons.workspaces.active = accent;
-      theme.bar.buttons.workspaces.available = accent-alt;
-      theme.bar.buttons.workspaces.occupied = accent-alt;
+      # theme.bar.buttons.workspaces.hover = accent-alt;
+      # theme.bar.buttons.workspaces.active = accent;
+      # theme.bar.buttons.workspaces.available = accent-alt;
+      # theme.bar.buttons.workspaces.occupied = accent-alt;
+      #
+      # theme.bar.menus.background = background;
+      # theme.bar.menus.cards = background-alt;
+      # theme.bar.menus.label = foreground;
+      # theme.bar.menus.text = foreground;
+      # theme.bar.menus.border.color = accent;
+      # theme.bar.menus.popover.text = foreground;
+      # theme.bar.menus.popover.background = background-alt;
+      # theme.bar.menus.listitems.active = accent;
+      # theme.bar.menus.icons.active = accent;
+      # theme.bar.menus.switch.enabled = accent;
+      # theme.bar.menus.check_radio_button.active = accent;
+      # theme.bar.menus.buttons.default = accent;
+      # theme.bar.menus.buttons.active = accent;
+      # theme.bar.menus.iconbuttons.active = accent;
+      # theme.bar.menus.progressbar.foreground = accent;
+      # theme.bar.menus.slider.primary = accent;
+      # theme.bar.menus.tooltip.background = background-alt;
+      # theme.bar.menus.tooltip.text = foreground;
+      # theme.bar.menus.dropdownmenu.background = background-alt;
+      # theme.bar.menus.dropdownmenu.text = foreground;
 
-      theme.bar.menus.background = background;
-      theme.bar.menus.cards = background-alt;
-      theme.bar.menus.label = foreground;
-      theme.bar.menus.text = foreground;
-      theme.bar.menus.border.color = accent;
-      theme.bar.menus.popover.text = foreground;
-      theme.bar.menus.popover.background = background-alt;
-      theme.bar.menus.listitems.active = accent;
-      theme.bar.menus.icons.active = accent;
-      theme.bar.menus.switch.enabled = accent;
-      theme.bar.menus.check_radio_button.active = accent;
-      theme.bar.menus.buttons.default = accent;
-      theme.bar.menus.buttons.active = accent;
-      theme.bar.menus.iconbuttons.active = accent;
-      theme.bar.menus.progressbar.foreground = accent;
-      theme.bar.menus.slider.primary = accent;
-      theme.bar.menus.tooltip.background = background-alt;
-      theme.bar.menus.tooltip.text = foreground;
-      theme.bar.menus.dropdownmenu.background = background-alt;
-      theme.bar.menus.dropdownmenu.text = foreground;
-
-      theme.bar.background =
-        background
-        + (
-          if transparentButtons && transparent
-          then "00"
-          else ""
-        );
-      theme.bar.buttons.text =
-        if transparent && transparentButtons
-        then foregroundOnWallpaper
-        else foreground;
-      theme.bar.buttons.background =
-        (
-          if transparent
-          then background
-          else background-alt
-        )
-        + (
-          if transparentButtons
-          then "00"
-          else ""
-        );
-      theme.bar.buttons.icon = accent;
-
-      theme.bar.buttons.notifications.background = background-alt;
-      theme.bar.buttons.hover = background;
-      theme.bar.buttons.notifications.hover = background;
-      theme.bar.buttons.notifications.total = accent;
-      theme.bar.buttons.notifications.icon = accent;
-
-      theme.osd.bar_color = accent;
-      theme.osd.bar_overflow_color = accent-alt;
-      theme.osd.icon = background;
-      theme.osd.icon_container = accent;
-      theme.osd.label = accent;
-      theme.osd.bar_container = background-alt;
-
-      theme.bar.menus.menu.media.background.color = background-alt;
-      theme.bar.menus.menu.media.card.color = background-alt;
-
-      theme.notification.background = background-alt;
-      theme.notification.actions.background = accent;
-      theme.notification.actions.text = foreground;
-      theme.notification.label = accent;
-      theme.notification.border = background-alt;
-      theme.notification.text = foreground;
-      theme.notification.labelicon = accent;
-      theme.notification.close_button.background = background-alt;
-      theme.notification.close_button.label = "#f38ba8";
+      # theme.bar.background =
+      #   background
+      #   + (
+      #     if transparentButtons && transparent
+      #     then "00"
+      #     else ""
+      #   );
+      # theme.bar.buttons.text =
+      #   if transparent && transparentButtons
+      #   then foregroundOnWallpaper
+      #   else foreground;
+      # theme.bar.buttons.background =
+      #   (
+      #     if transparent
+      #     then background
+      #     else background-alt
+      #   )
+      #   + (
+      #     if transparentButtons
+      #     then "00"
+      #     else ""
+      #   );
+      # theme.bar.buttons.icon = accent;
+      #
+      # theme.bar.buttons.notifications.background = background-alt;
+      # theme.bar.buttons.hover = background;
+      # theme.bar.buttons.notifications.hover = background;
+      # theme.bar.buttons.notifications.total = accent;
+      # theme.bar.buttons.notifications.icon = accent;
+      #
+      # theme.osd.bar_color = accent;
+      # theme.osd.bar_overflow_color = accent-alt;
+      # theme.osd.icon = background;
+      # theme.osd.icon_container = accent;
+      # theme.osd.label = accent;
+      # theme.osd.bar_container = background-alt;
+      #
+      # theme.bar.menus.menu.media.background.color = background-alt;
+      # theme.bar.menus.menu.media.card.color = background-alt;
+      #
+      # theme.notification.background = background-alt;
+      # theme.notification.actions.background = accent;
+      # theme.notification.actions.text = foreground;
+      # theme.notification.label = accent;
+      # theme.notification.border = background-alt;
+      # theme.notification.text = foreground;
+      # theme.notification.labelicon = accent;
+      # theme.notification.close_button.background = background-alt;
+      # theme.notification.close_button.label = "#f38ba8";
     };
   };
 }
