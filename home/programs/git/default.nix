245b28ad7537e88e0d7943a3017ea20006696144
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-10-25 22:03:13 +0200

    update git config
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -5,8 +5,6 @@
 in {
   programs.git = {
     enable = true;
-    userName = username;
-    userEmail = email;
     ignores = [
       ".cache/"
       ".DS_Store"
@@ -19,37 +17,39 @@ in {
       "result"
       "result-*"
     ];
-    extraConfig = {
+    settings = {
+      user.name = username;
+      user.email = email;
       init.defaultBranch = "main";
       pull.rebase = "false";
       push.autoSetupRemote = true;
       color.ui = "1";
-    };
-    aliases = {
-      essa = "push --force";
-      co = "checkout";
-      fuck = "commit --amend -m";
-      c = "commit -m";
-      ca = "commit -am";
-      forgor = "commit --amend --no-edit";
-      graph = "log --all --decorate --graph --oneline";
-      oops = "checkout --";
-      l = "log";
-      r = "rebase";
-      s = "status --short";
-      ss = "status";
-      d = "diff";
-      ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
-      pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
-      af = "!git add $(git ls-files -m -o --exclude-standard | sk -m)";
-      st = "status";
-      br = "branch";
-      df = "!git hist | peco | awk '{print $2}' | xargs -I {} git diff {}^ {}";
-      hist = ''
-        log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all'';
-      llog = ''
-        log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative'';
-      edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
+      alias = {
+        essa = "push --force";
+        co = "checkout";
+        fuck = "commit --amend -m";
+        c = "commit -m";
+        ca = "commit -am";
+        forgor = "commit --amend --no-edit";
+        graph = "log --all --decorate --graph --oneline";
+        oops = "checkout --";
+        l = "log";
+        r = "rebase";
+        s = "status --short";
+        ss = "status";
+        d = "diff";
+        ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
+        pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
+        af = "!git add $(git ls-files -m -o --exclude-standard | sk -m)";
+        st = "status";
+        br = "branch";
+        df = "!git hist | peco | awk '{print $2}' | xargs -I {} git diff {}^ {}";
+        hist = ''
+          log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all'';
+        llog = ''
+          log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative'';
+        edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; hx `f`";
+      };
     };
   };
 }
--- home/programs/git/signing.nix
+++ home/programs/git/signing.nix
@@ -3,7 +3,7 @@
   # Obviously, change this to your own SSH key.
   home.file.".ssh/allowed_signers".text = "* ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIINhWby7lUUXQNKbRu9/UOrGjWDf3fvoAwGHomWv/+lL";
 
-  programs.git.extraConfig = {
+  programs.git.settings = {
     commit.gpgsign = true;
     gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
     gpg.format = "ssh";
