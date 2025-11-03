c9e072899b8bcec6c45cbf5c83dd1acea6f56cbf
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-11-03 15:49:02 +0100

    change kitty config & switch from alacritty to kitty
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -3,7 +3,8 @@
   programs.kitty = {
     enable = true;
     keybindings = {
-      "ctrl+shift+tab" = "new_tab";
+      "ctrl+shift+tab" = "new_tab_with_cwd";
+      "ctrl+shift+enter" = "new_window_with_cwd";
       "ctrl+j" = ''kitten pass_keys.py bottom ctrl+j'';
       "ctrl+k" = ''kitten pass_keys.py top    ctrl+k'';
       "ctrl+h" = ''kitten pass_keys.py left   ctrl+h'';
