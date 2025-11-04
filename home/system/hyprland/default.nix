c09899e3653c70627706d467ef6de12f7aa4c43b
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-11-04 16:15:01 +0100

    remove nextcloud
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- home/system/hyprland/default.nix
+++ home/system/hyprland/default.nix
@@ -64,7 +64,6 @@ in {
       exec-once = [
         "dbus-update-activation-environment --systemd --all &"
         "systemctl --user enable --now hyprpaper.service &"
-        "systemctl --user enable --now nextcloud-client.service  &"
       ];
 
       monitor = [
