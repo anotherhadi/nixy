52ee08e13bd8f9b46826045dcc5d2fa90511cb62
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-01-15 18:48:18 +0100

    Firewall allow port 53: DNS requests
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- server-modules/adguardhome.nix
+++ server-modules/adguardhome.nix
@@ -6,5 +6,10 @@
     port = 3000;
   };
 
+  networking.firewall = {
+    allowedTCPPorts = [53];
+    allowedUDPPorts = [53];
+  };
+
   services.cloudflared.tunnels."f7c8f777-a36c-4b9a-b6e3-6a112bd43e73".ingress."adguard.hadi.diy" = "http://localhost:${toString config.services.adguardhome.port}";
 }
