d61e378405f8c0100c287cd455b4fca9fca605d2
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-02-07 12:31:29 +0100

    remove blueman
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -1,9 +1,7 @@
 # Bluetooth configuration for NixOS
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [blueman];
+{
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
-  services.blueman.enable = true;
 }
