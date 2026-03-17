fabc66d025454591ea86b8e4b0a9c54c4553109b
pph <pph@pph.pph>
2026-03-17 11:30:00 +0100

    edit ghostty configuration
    Signed-off-by: pph <pph@pph.pph>

--- home/programs/ghostty/default.nix
+++ home/programs/ghostty/default.nix
@@ -7,14 +7,18 @@
       window-padding-x = 10;
       confirm-close-surface = false;
       window-padding-y = 10;
+      clipboard-read = "allow";
+      clipboard-write = "allow";
+      copy-on-select = "clipboard";
+      app-notifications = false;
       keybind = [
         "ctrl+j=goto_split:left"
         "ctrl+i=goto_split:up"
         "ctrl+k=goto_split:down"
         "ctrl+l=goto_split:right"
-        "shift+ctrl+j=new_split:left"
-        "shift+ctrl+i=new_split:up"
-        "shift+ctrl+k=new_split:down"
+        "shift+ctrl+h=new_split:left"
+        "shift+ctrl+j=new_split:down"
+        "shift+ctrl+k=new_split:up"
         "shift+ctrl+l=new_split:right"
         "shift+ctrl+tab=new_tab"
       ];
