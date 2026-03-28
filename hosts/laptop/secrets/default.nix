a9ef1dacd1141f839c5336d93f987204a2ea42b7
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-03-28 18:59:02 +0100

    change gitlab secret
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- hosts/laptop/secrets/default.nix
+++ hosts/laptop/secrets/default.nix
@@ -13,6 +13,7 @@
     secrets = {
       sshconfig = {path = "/home/hadi/.ssh/config";};
       github-key = {path = "/home/hadi/.ssh/github";};
+      gitlab-key = {path = "/home/hadi/.ssh/gitlab";};
       jack-key = {path = "/home/hadi/.ssh/jack";};
       signing-key = {path = "/home/hadi/.ssh/key";};
       signing-pub-key = {path = "/home/hadi/.ssh/key.pub";};
