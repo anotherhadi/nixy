de3b1f96c8434b89a9f1be3937c59462d058e383
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-03-30 20:00:00 +0200

    update blog config
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- server-modules/blog.nix
+++ server-modules/blog.nix
@@ -4,23 +4,41 @@
   ...
 }: {
   services = {
-    nginx.virtualHosts."blog.local" = {
-      root = "${inputs.blog.packages.${pkgs.system}.default}/share/blog";
-      listen = [
-        {
-          addr = "127.0.0.1";
-          port = 8758;
-        }
-      ];
-      locations."/" = {
-        tryFiles = "$uri $uri/ /index.html";
+    nginx.virtualHosts = {
+      "blog.local" = {
+        root = "${inputs.blog.packages.${pkgs.system}.default}/share/blog";
+        listen = [
+          {
+            addr = "127.0.0.1";
+            port = 8758;
+          }
+        ];
+        locations."/" = {
+          tryFiles = "$uri $uri/ /index.html";
+        };
+        extraConfig = ''
+          port_in_redirect off;
+          absolute_redirect off;
+          add_header Content-Security-Policy "default-src 'self'; script-src 'self' 'unsafe-inline' data: https://umami.hadi.icu; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://git.hadi.icu; connect-src 'self' https://umami.hadi.icu;" always;
+        '';
       };
-      extraConfig = ''
-        port_in_redirect off;
-        absolute_redirect off;
-      '';
+
+      "www.blog.local" = {
+        listen = [
+          {
+            addr = "127.0.0.1";
+            port = 8189;
+          }
+        ];
+        extraConfig = ''
+          return 301 https://hadi.icu$request_uri;
+        '';
+      };
     };
-    cloudflared.tunnels."a1dfa315-7fc3-4a65-8c02-8387932c35c3".ingress."hadi.icu" = "http://127.0.0.1:8758";
-    cloudflared.tunnels."a1dfa315-7fc3-4a65-8c02-8387932c35c3".ingress."www.hadi.icu" = "http://127.0.0.1:8758";
+
+    cloudflared.tunnels."a1dfa315-7fc3-4a65-8c02-8387932c35c3".ingress = {
+      "hadi.icu" = "http://127.0.0.1:8758";
+      "www.hadi.icu" = "http://127.0.0.1:8189";
+    };
   };
 }
