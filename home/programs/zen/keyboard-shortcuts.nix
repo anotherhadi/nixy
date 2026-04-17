cef215e013e48014af9da2152b51226d5d69107c
pph <pph@pph.pph>
2026-04-17 13:40:29 +0200

    move shortcuts
    Signed-off-by: pph <pph@pph.pph>

---
+++ home/programs/zen/keyboard-shortcuts.nix
@@ -0,0 +1,299 @@
+[
+  # Remaps
+  {
+    id = "focusURLBar";
+    key = " ";
+    modifiers.control = true;
+  }
+  {
+    id = "key_newNavigator";
+    key = "n";
+    modifiers.accel = true;
+  }
+  {
+    id = "viewBookmarksToolbarKb";
+    key = "b";
+    modifiers = {
+      accel = true;
+      shift = true;
+    };
+  }
+  {
+    id = "key_findAgain";
+    disabled = true;
+  }
+  {
+    id = "key_findPrevious";
+    disabled = true;
+  }
+  {
+    id = "key_privatebrowsing";
+    key = "n";
+    modifiers = {
+      accel = true;
+      shift = true;
+    };
+  }
+  {
+    id = "key_gotoHistory";
+    key = "h";
+    modifiers = {
+      accel = true;
+      shift = true;
+    };
+  }
+  {
+    id = "goBackKb";
+    key = "h";
+    modifiers.accel = true;
+  }
+  {
+    id = "goForwardKb";
+    key = "l";
+    modifiers.accel = true;
+  }
+
+  # Disabled shortcuts
+  {
+    id = "printKb";
+    disabled = true;
+  }
+  {
+    id = "zen-close-all-unpinned-tabs";
+    disabled = true;
+  }
+  {
+    id = "zen-new-empty-split-view";
+    disabled = true;
+  }
+  {
+    id = "zen-split-view-unsplit";
+    disabled = true;
+  }
+  {
+    id = "zen-split-view-horizontal";
+    disabled = true;
+  }
+  {
+    id = "zen-split-view-vertical";
+    disabled = true;
+  }
+  {
+    id = "zen-split-view-grid";
+    disabled = true;
+  }
+  {
+    id = "zen-glance-expand";
+    disabled = true;
+  }
+  {
+    id = "zen-toggle-pin-tab";
+    disabled = true;
+  }
+  {
+    id = "zen-toggle-sidebar";
+    disabled = true;
+  }
+  {
+    id = "zen-new-unsynced-window";
+    disabled = true;
+  }
+  {
+    id = "key_closeWindow";
+    disabled = true;
+  }
+  {
+    id = "key_quitApplication";
+    disabled = true;
+  }
+  {
+    id = "key_search";
+    disabled = true;
+  }
+  {
+    id = "key_search2";
+    disabled = true;
+  }
+  {
+    id = "focusURLBar2";
+    disabled = true;
+  }
+  {
+    id = "key_savePage";
+    disabled = true;
+  }
+  {
+    id = "key_togglePictureInPicture";
+    disabled = true;
+  }
+  {
+    id = "showAllHistoryKb";
+    disabled = true;
+  }
+  {
+    id = "addBookmarkAsKb";
+    disabled = true;
+  }
+  {
+    id = "manBookmarkKb";
+    disabled = true;
+  }
+  {
+    id = "viewBookmarksSidebarKb";
+    key = "b";
+    modifiers = {
+      accel = true;
+    };
+  }
+  {
+    id = "key_toggleMute";
+    disabled = true;
+  }
+  {
+    id = "key_switchTextDirection";
+    disabled = true;
+  }
+  {
+    id = "key_screenshot";
+    disabled = true;
+  }
+  {
+    id = "key_viewInfo";
+    disabled = true;
+  }
+  {
+    id = "key_toggleToolbox";
+    disabled = true;
+  }
+  {
+    id = "key_browserToolbox";
+    disabled = true;
+  }
+  {
+    id = "key_browserConsole";
+    disabled = true;
+  }
+  {
+    id = "key_responsiveDesignMode";
+    disabled = true;
+  }
+  {
+    id = "key_inspector";
+    disabled = true;
+  }
+  {
+    id = "key_webconsole";
+    key = "i";
+    modifiers = {
+      accel = true;
+      shift = true;
+    };
+  }
+  {
+    id = "key_jsdebugger";
+    disabled = true;
+  }
+  {
+    id = "key_netmonitor";
+    disabled = true;
+  }
+  {
+    id = "key_styleeditor";
+    disabled = true;
+  }
+  {
+    id = "key_performance";
+    disabled = true;
+  }
+  {
+    id = "key_storage";
+    disabled = true;
+  }
+  {
+    id = "key_dom";
+    disabled = true;
+  }
+  {
+    id = "key_accessibility";
+    disabled = true;
+  }
+  {
+    id = "key_openDownloads";
+    disabled = true;
+  }
+  {
+    id = "key_openAddons";
+    disabled = true;
+  }
+  {
+    id = "key_reload";
+    key = "r";
+    modifiers = {
+      accel = true;
+    };
+  }
+  {
+    id = "key_reload2";
+    disabled = true;
+  }
+  {
+    id = "key_reload_skip_cache";
+    key = "r";
+    modifiers = {
+      accel = true;
+      shift = true;
+    };
+  }
+  {
+    id = "key_reload_skip_cache2";
+    disabled = true;
+  }
+  {
+    id = "key_enterFullScreen";
+    disabled = true;
+  }
+  {
+    id = "key_exitFullScreen";
+    disabled = true;
+  }
+  {
+    id = "key_aboutProcesses";
+    disabled = true;
+  }
+  {
+    id = "viewGenaiChatSidebarKb";
+    disabled = true;
+  }
+  {
+    id = "toggleSidebarKb";
+    disabled = true;
+  }
+  {
+    id = "key_showAllTabs";
+    disabled = true;
+  }
+  {
+    id = "key_sanitize";
+    disabled = true;
+  }
+  {
+    id = "key_wrCaptureCmd";
+    disabled = true;
+  }
+  {
+    id = "key_wrToggleCaptureSequenceCmd";
+    disabled = true;
+  }
+  {
+    id = "goHome";
+    disabled = true;
+  }
+  {
+    id = "goBackKb2";
+    disabled = true;
+  }
+  {
+    id = "goForwardKb2";
+    disabled = true;
+  }
+]
