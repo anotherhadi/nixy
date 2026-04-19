e2a6572b3becbab04f860a1cb070b4330091250b
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-04-19 17:38:03 +0200

    Add Proton Mail & Calendar shortcuts
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- home/programs/proton/default.nix
+++ home/programs/proton/default.nix
@@ -15,5 +15,21 @@
       categories = ["Utility"];
       terminal = false;
     };
+    "Proton Calendar" = {
+      name = "Proton Calendar";
+      exec = "${pkgs.qutebrowser}/bin/qutebrowser \"https://calendar.proton.me\"";
+      icon = "proton-calendar";
+      type = "Application";
+      categories = ["Utility"];
+      terminal = false;
+    };
+    "Proton Mail" = {
+      name = "Proton Mail";
+      exec = "${pkgs.qutebrowser}/bin/qutebrowser \"https://mail.proton.me/\"";
+      icon = "proton-mail";
+      type = "Application";
+      categories = ["Utility"];
+      terminal = false;
+    };
   };
 }
