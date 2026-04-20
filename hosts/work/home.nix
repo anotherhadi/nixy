c06bdf96879f9dcfc7fdd116d1e190d2c1f05444
Hadi <hadi@example.com>
2026-04-20 14:05:09 +0200

    Init the work config v2
    Signed-off-by: Hadi <hadi@example.com>

---
+++ hosts/work/home.nix
@@ -0,0 +1,43 @@
+{config, ...}: {
+  imports = [
+    # Programs
+    ../../home/programs/brave
+    ../../home/programs/proton
+    ../../home/programs/ghostty
+    ../../home/programs/nvf
+    ../../home/programs/shell
+    ../../home/programs/git
+    ../../home/programs/git/lazygit.nix
+    ../../home/programs/spicetify
+    ../../home/programs/thunar
+    ../../home/programs/nixy
+    ../../home/programs/nightshift
+    ../../home/programs/qutebrowser
+    ../../home/programs/nix-utils
+
+    ../../home/programs/group/basic-apps.nix
+    ../../home/programs/group/cybersecurity.nix
+    ../../home/programs/group/dev.nix
+    ../../home/programs/group/misc.nix
+
+    # System (Desktop environment like stuff)
+    ../../home/system/hyprland
+    ../../home/system/caelestia-shell
+    ../../home/system/hyprpaper
+    ../../home/system/mime
+    ../../home/system/udiskie
+
+    ./variables.nix # Mostly user-specific configuration
+    ./secrets # CHANGEME: You should probably remove this line, this is where I store my secrets
+  ];
+
+  home = {
+    inherit (config.var) username;
+    homeDirectory = "/home/" + config.var.username;
+
+    # Don't touch this
+    stateVersion = "24.05";
+  };
+
+  programs.home-manager.enable = true;
+}
---
+++ hosts/work/secrets/default.nix
@@ -0,0 +1,46 @@
+# Those are my secrets, encrypted with sops
+# You shouldn't import this file, unless you edit it
+{
+  inputs,
+  pkgs,
+  config,
+  ...
+}: let
+  home = config.home.homeDirectory;
+in {
+  imports = [inputs.sops-nix.homeManagerModules.sops];
+
+  sops = {
+    age.keyFile = "${home}/.config/sops/age/keys.txt";
+    defaultSopsFile = ./secrets.yaml;
+    secrets = {
+      ssh-config = {path = "${home}/.ssh/config";};
+      github-key = {path = "${home}/.ssh/github";};
+      gitlab-key = {path = "${home}/.ssh/gitlab";};
+    };
+  };
+
+  home.file.".config/nixos/.sops.yaml".text = ''
+    keys:
+      - &primary age12yvtj49pfh3fqzqflscm0ek4yzrjhr6cqhn7x89gdxnlykq0xudq5c7334
+      - &work age1c8pawdsxptfslgrz2c56s39mrtnjzc5mm3hfzgr2wdwu2v6vfsdsupjsq6
+    creation_rules:
+      - path_regex: hosts/laptop/secrets/secrets.yaml$
+        key_groups:
+          - age:
+            - *primary
+      - path_regex: hosts/server/secrets/secrets.yaml$
+        key_groups:
+          - age:
+            - *primary
+      - path_regex: hosts/work/secrets/secrets.yaml$
+        key_groups:
+          - age:
+            - *work
+  '';
+
+  systemd.user.services.mbsync.Unit.After = ["sops-nix.service"];
+  home.packages = with pkgs; [sops age];
+
+  wayland.windowManager.hyprland.settings.exec-once = ["systemctl --user start sops-nix"];
+}
