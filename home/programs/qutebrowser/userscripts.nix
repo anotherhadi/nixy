f95f09b5fe24ebb21d9d542874d6d8ab4d58642b
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-04-27 20:31:47 +0200

    Qutebrowser greasemonkey as flake now
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- home/programs/qutebrowser/userscripts.nix
+++ home/programs/qutebrowser/userscripts.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   xdg.dataFile = {
     # Startpage: hide sponsored results (custom script, no upstream)
     "qutebrowser/greasemonkey/startpage-no-ads.user.js".text = ''
@@ -22,46 +26,12 @@
       }).observe(document, { childList: true, subtree: true });
     '';
 
-    # Return YouTube Dislike: restore dislike counts on YouTube
-    "qutebrowser/greasemonkey/return-youtube-dislike.user.js".source = pkgs.fetchurl {
-      url = "https://update.greasyfork.org/scripts/436115/Return%20YouTube%20Dislike.user.js";
-      hash = "sha256-P7dK3v1WbSQaJUo73iHrezkXE+6BOdIuDk/D6GJwwbM=";
-    };
-
-    # SponsorBlock Lite: auto-skip sponsors on YouTube
-    "qutebrowser/greasemonkey/sponsorblock-lite.user.js".source = pkgs.fetchurl {
-      url = "https://update.greasyfork.org/scripts/560869/SponsorBlock%20Lite.user.js";
-      hash = "sha256-8DTIRMn+cy/gZeeHa6xJDomQ5QN3lnaK0DG5ZcS5d00=";
-    };
-
-    # Don't Track Me Google: remove Google tracking redirects
-    "qutebrowser/greasemonkey/dont-track-me-google.user.js".source = pkgs.fetchurl {
-      url = "https://update.greasyfork.org/scripts/428243/Don%27t%20track%20me%20Google.user.js";
-      hash = "sha256-yEjBZprSjHyDRpd+TJ1vUsSYHrwLspQOztpKunBLPig=";
-    };
-
-    # I don't care about cookies: auto-dismiss cookie banners
-    "qutebrowser/greasemonkey/i-dont-care-about-cookies.user.js".source = pkgs.fetchurl {
-      url = "https://update.greasyfork.org/scripts/522645/I%20don%27t%20care%20about%20cookies.user.js";
-      hash = "sha256-Ij7HyBfWemAO0EAGKYxWPPv7OX5twNtGKKPGhOAxM9w=";
-    };
-
-    # TrackingTokenStripper: remove tracking params from URLs (utm_*, fbclid, etc.)
-    "qutebrowser/greasemonkey/tracking-token-stripper.user.js".source = pkgs.fetchurl {
-      url = "https://github.com/doggy8088/TrackingTokenStripper/raw/refs/heads/master/TrackingTokenStripper.user.js";
-      hash = "sha256-EX8xN2Vd8SE/RvMcF/YSGN4Epa5cm355IeD9agTP2h4=";
-    };
-
-    # Bypass Paywalls Clean: bypass news site paywalls (Le Monde, NY Times, etc.)
-    "qutebrowser/greasemonkey/bypass-paywalls-clean.user.js".source = pkgs.fetchurl {
-      url = "https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters/blob/raw?file=userscript/bpc.en.user.js";
-      hash = "sha256-dUgwBkJi5Jrtpw5HJydRjY9xBpZXyD0ZtA/+hDzF97s=";
-    };
-
-    # Anti-Adblock Fuckoff: remove anti-adblock modals and restore scroll
-    "qutebrowser/greasemonkey/anti-adblock-fuckoff.user.js".source = pkgs.fetchurl {
-      url = "https://update.greasyfork.org/scripts/397070/Anti-AdBlocker%20Fuckoff.user.js";
-      hash = "sha256-vFeWxqMg0gPHP7mGNZO9e9Me/2Z81biR5JEXC/Ct4fA=";
-    };
+    "qutebrowser/greasemonkey/return-youtube-dislike.user.js".source = inputs.qs-return-youtube-dislike;
+    "qutebrowser/greasemonkey/sponsorblock-lite.user.js".source = inputs.qs-sponsorblock-lite;
+    "qutebrowser/greasemonkey/dont-track-me-google.user.js".source = inputs.qs-dont-track-me-google;
+    "qutebrowser/greasemonkey/i-dont-care-about-cookies.user.js".source = inputs.qs-i-dont-care-about-cookies;
+    "qutebrowser/greasemonkey/tracking-token-stripper.user.js".source = inputs.qs-tracking-token-stripper;
+    "qutebrowser/greasemonkey/bypass-paywalls-clean.user.js".source = inputs.qs-bypass-paywalls-clean;
+    "qutebrowser/greasemonkey/anti-adblock-fuckoff.user.js".source = inputs.qs-anti-adblock-fuckoff;
   };
 }
