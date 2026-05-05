685e88700c2d66ba76d66335b56977a726461860
Hadi <hadi@example.com>
2026-05-05 09:54:52 +0200

    Add usbguard-tui
    Signed-off-by: Hadi <hadi@example.com>

--- home/programs/group/misc.nix
+++ home/programs/group/misc.nix
@@ -1,9 +1,14 @@
-{pkgs-stable, ...}: {
+{
+  pkgs-stable,
+  inputs,
+  ...
+}: {
   home.packages = with pkgs-stable; [
     peaclock
     cbonsai
     pipes
     cmatrix
     fastfetch
+    inputs.usbguard-tui.packages.${system}.default
   ];
 }
