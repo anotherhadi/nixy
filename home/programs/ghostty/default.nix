5dff7ceb12231cf6f5ee36398b37b651dc3871a1
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-05-21 13:21:36 +0200

    remove useless shortcuts
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- home/programs/ghostty/default.nix
+++ home/programs/ghostty/default.nix
@@ -1,13 +1,11 @@
-{ pkgs, ... }:
-let
+{pkgs, ...}: let
   cursorShaders = pkgs.fetchFromGitHub {
     owner = "sahaj-b";
     repo = "ghostty-cursor-shaders";
     rev = "06d4e90fb5410e9c4d0b3131584060adddf89406";
     hash = "sha256-G/UIr1bKnxn1AcHl/4FL/jou6b7M2VeREslYVELxdmw=";
   };
-in
-{
+in {
   home.sessionVariables = {
     TERMINAL = "ghostty";
     TERM = "ghostty";
@@ -30,14 +28,14 @@ in
       custom-shader = "${cursorShaders}/cursor_warp.glsl";
       custom-shader-animation = "always";
       keybind = [
-        "ctrl+j=goto_split:left"
-        "ctrl+i=goto_split:up"
-        "ctrl+k=goto_split:down"
-        "ctrl+l=goto_split:right"
-        "shift+ctrl+h=new_split:left"
-        "shift+ctrl+j=new_split:down"
-        "shift+ctrl+k=new_split:up"
-        "shift+ctrl+l=new_split:right"
+        # "ctrl+j=goto_split:left"
+        # "ctrl+i=goto_split:up"
+        # "ctrl+k=goto_split:down"
+        # "ctrl+l=goto_split:right"
+        # "shift+ctrl+h=new_split:left"
+        # "shift+ctrl+j=new_split:down"
+        # "shift+ctrl+k=new_split:up"
+        # "shift+ctrl+l=new_split:right"
         "shift+ctrl+tab=new_tab"
       ];
     };
