5f72e16819d997fc3b782ae3b4087e72585a0f6d
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-05-22 09:20:58 +0200

    add spilltea, sttr, httpie & trufflehog
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- home/programs/group/cybersecurity.nix
+++ home/programs/group/cybersecurity.nix
@@ -1,9 +1,13 @@
 {
+  inputs,
   pkgs-stable,
   pkgs,
   ...
 }: {
   home.packages = with pkgs-stable; [
+    httpie
+    trufflehog
+    sttr
     wireshark
     nmap
     john
@@ -19,12 +23,14 @@
     nuclei
 
     # Web
+    inputs.spilltea.packages.${pkgs.system}.default
     caido
     nuclei
     gobuster
     dirb
     ffuf
     sqlmap
+    katana
 
     # Utils
     inetutils
@@ -41,22 +47,18 @@
   ];
 
   home.file = {
-    "Cyber/wordlists/seclists" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "danielmiessler";
-        repo = "SecLists";
-        rev = "47aa98053fce8670c6489e4f6fb57dc34881d8bd";
-        hash = "sha256-ojXLB/TXap1Qi0MtV5/WJlJpjNIDYx/sLY2KHm0uFKI=";
-      };
+    "Cyber/wordlists/seclists".source = pkgs.fetchFromGitHub {
+      owner = "danielmiessler";
+      repo = "SecLists";
+      rev = "47aa98053fce8670c6489e4f6fb57dc34881d8bd";
+      hash = "sha256-ojXLB/TXap1Qi0MtV5/WJlJpjNIDYx/sLY2KHm0uFKI=";
     };
 
-    "Cyber/wordlists/fuzz4bounty" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "0xPugal";
-        repo = "fuzz4bounty";
-        rev = "a805f6b434033f054312da0ddc2bc3021cc9a3b0";
-        hash = "sha256-wMhx1CcxIyw2E7gJXQxXAFc7m5nYt0FXNkoTZqFMQh4=";
-      };
+    "Cyber/wordlists/fuzz4bounty".source = pkgs.fetchFromGitHub {
+      owner = "0xPugal";
+      repo = "fuzz4bounty";
+      rev = "a805f6b434033f054312da0ddc2bc3021cc9a3b0";
+      hash = "sha256-wMhx1CcxIyw2E7gJXQxXAFc7m5nYt0FXNkoTZqFMQh4=";
     };
 
     # RULES
@@ -67,11 +69,9 @@
       hash = "sha256-h7MeymIXS/6wlPRt4lzsIEqOOssP0lDx9nQip65cwZw=";
     };
 
-    "Cyber/wordlists/rules/best64.rule" = {
-      source = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/CarlosLannister/OwadeReborn/refs/heads/master/owade/fileAnalyze/hashcatLib/best64.rule%7E";
-        hash = "sha256-T0XkMHJZQiy63/j25nGTkaiDFjc+blmgEVGTAFgVylU=";
-      };
+    "Cyber/wordlists/rules/best64.rule".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/CarlosLannister/OwadeReborn/refs/heads/master/owade/fileAnalyze/hashcatLib/best64.rule%7E";
+      hash = "sha256-T0XkMHJZQiy63/j25nGTkaiDFjc+blmgEVGTAFgVylU=";
     };
   };
 }
