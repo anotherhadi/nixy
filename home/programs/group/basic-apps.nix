c76f503dcc1376dd88202010f5f60aeee04ef6d5
Hadi <hadi@example.com>
2026-06-04 16:20:41 +0200

    add TUIs
    Signed-off-by: Hadi <hadi@example.com>

--- home/programs/group/basic-apps.nix
+++ home/programs/group/basic-apps.nix
@@ -14,6 +14,11 @@
     signal-desktop # Messaging app
     librewolf # Backup browser
 
+    # I love TUIs
     caligula # User-friendly, lightweight TUI for disk imaging (ISO, USB BOOT)
+    browsh # A modern text-based browser that renders anything that a modern browser can; including HTML5, CSS3, JS, video and WebGL. It runs in a terminal and can be used remotely over SSH.
+    pastel # Command-line tool to generate, analyze, convert and manipulate colors
+    dysk # A terminal-based disk usage analyzer, similar to 'du' but with a more user-friendly interface and additional features.
+    wikiman # Offline search engine for manual pages
   ];
 }
