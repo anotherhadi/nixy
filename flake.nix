ac008183bdd4a32abb81ea080db1e891c83162fc
Hadi <hadi@example.com>
2026-08-17 11:00:37 +0200

    add the flake to work
    Signed-off-by: Hadi <hadi@example.com>

--- flake.nix
+++ flake.nix
@@ -93,6 +93,7 @@
         apps.${system}.nvim = inputs.nvf-config.apps.${system}.nvim;
         nixosConfigurations = {
           h-laptop = import ./hosts/laptop/flake.nix args;
+          h-work = import ./hosts/work/flake.nix args;
           jack = import ./hosts/server/flake.nix args;
         };
         devShells = forAllSystems (system: pkgs: {
