b024ce0b4d8fe59e5b63686ba354ce0e38b2d53a
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-09-05 20:29:54 +0200

    fix for my g5 dock
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,5 +1,5 @@
 # Audio configuration for NixOS using PipeWire
-{
+{pkgs, ...}: {
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -8,4 +8,11 @@
     jack.enable = true;
   };
   security.rtkit.enable = true;
+
+  environment.systemPackages = [pkgs.alsa-utils];
+
+  # Fix for the HP USB-C Dock G5's
+  services.udev.extraRules = ''
+    SUBSYSTEM=="sound", KERNEL=="controlC*", ATTRS{idVendor}=="03f0", ATTRS{idProduct}=="056b", RUN+="${pkgs.bash}/bin/bash -c '${pkgs.alsa-utils}/bin/amixer -c $(echo %k | sed s/controlC//) sset Headphones unmute'"
+  '';
 }
