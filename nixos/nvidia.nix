2192f3aa7c1d0d94a8cbe4be25dd9ed9a19fc005
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-09-20 21:45:33 +0200

    update cache url
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -62,9 +62,11 @@ in {
 
   # Nix cache for CUDA
   nix.settings = {
-    substituters = ["https://cuda-maintainers.cachix.org"];
+    substituters = [
+      "https://cache.nixos-cuda.org"
+    ];
     trusted-public-keys = [
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
   };
 
